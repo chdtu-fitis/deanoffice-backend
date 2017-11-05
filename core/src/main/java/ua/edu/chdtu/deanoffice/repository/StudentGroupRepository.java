d8d70936c83f72dd08f446127daef2f8bd62be0d
OlegSamardak <os1998os@ukr.net>
2017-11-05 23:48:06 +0200

    Created StudentGroupRepository. Find by specialization.

---
+++ core/src/main/java/ua/edu/chdtu/deanoffice/repository/StudentGroupRepository.java
@@ -0,0 +1,17 @@
+package ua.edu.chdtu.deanoffice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ua.edu.chdtu.deanoffice.entity.StudentGroup;
+
+import java.util.List;
+
+/**
+ * Created by os199 on 05.11.2017.
+ */
+public interface StudentGroupRepository extends JpaRepository<StudentGroup, Integer>{
+
+    @Query("SELECT sg.id, sg.name, sg.studySemesters FROM StudentGroup AS sg INNER JOIN sg.specialization ON sg.id = sg.specialization.id WHERE sg.active = 'T' and sg.specialization.id = :specId")
+    List<StudentGroup> findAllBySpecialization(@Param("specId") int specId);
+}
