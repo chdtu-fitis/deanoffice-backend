017d9c23e84f8d80760c0592b1430e961a1682b0
OlegSamardak <os1998os@ukr.net>
2017-11-06 11:25:31 +0200

    Created TeacherRepository. Finding all teachers by faculty id

--- core/src/main/java/ua/edu/chdtu/deanoffice/repository/CourseForGroupRepository.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/repository/CourseForGroupRepository.java
@@ -11,8 +11,10 @@ import java.util.List;
  * Created by os199 on 06.11.2017.
  */
 public interface CourseForGroupRepository extends JpaRepository<CourseForGroup, Integer> {
+
     @Query("select cfg.course from CourseForGroup as cfg " +
             "join cfg.studentGroup sg join sg.specialization spec join spec.faculty " +
             "where sg.active = 'T' and spec.faculty.id = :facultyId and spec.id = :specId")
     List<CourseForGroup> findAllBySpecialization(@Param("facultyId") int facultyId, @Param("specId") int specId);
+
 }
