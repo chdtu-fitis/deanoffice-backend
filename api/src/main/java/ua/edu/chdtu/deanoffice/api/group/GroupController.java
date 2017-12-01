501f72805f972610ee7e7c24a795282e4c471ba8
SashaKononenko <alex327021@gmail.com>
2017-12-01 17:30:28 +0200

    Data is not converted correctly.

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/group/GroupController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/group/GroupController.java
@@ -1,36 +1,55 @@
 package ua.edu.chdtu.deanoffice.api.group;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import ua.edu.chdtu.deanoffice.api.group.dto.GroupViews;
+import ua.edu.chdtu.deanoffice.api.group.dto.GroupDTO;
+import ua.edu.chdtu.deanoffice.entity.StudentGroup;
 import ua.edu.chdtu.deanoffice.entity.TestEntity;
+import ua.edu.chdtu.deanoffice.test.GroupService;
 import ua.edu.chdtu.deanoffice.test.TestService;
-import ua.edu.chdtu.deanoffice.webstarter.ErrorMessageResponse;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/groups")
 public class GroupController {
+
     @Autowired
-    private TestService testService;
+    private final GroupService groupService;
+    private ModelMapper modelMapper = new ModelMapper();
+    @Autowired
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
 
     @RequestMapping("/")
     @ResponseBody
     @JsonView(GroupViews.Name.class)
-    public TestEntity getGroups() {
-        TestEntity test = testService.getTest();
-        return test;
+    public List<GroupDTO> getGroups() {
+        List<StudentGroup> studentGroups = groupService.getGroups();
+        Type listType = new TypeToken<List<GroupDTO>>() {}.getType();
+        List<GroupDTO> groupDTOs = modelMapper.map(studentGroups,  listType);
+
+        return groupDTOs;
     }
 
-    @RequestMapping("{id}/courses")
+
+    /*@RequestMapping("{id}/courses")
     @ResponseBody
     public TestEntity getCourses(@PathVariable String id) {
         TestEntity test = testService.getTest();
         return test;
-    }
+    }*/
 
 }
--- api/src/main/java/ua/edu/chdtu/deanoffice/api/group/dto/GroupDTO.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/group/dto/GroupDTO.java
@@ -13,8 +13,10 @@ public class GroupDTO {
     public int creationYear;
     public char tuitionForm = 'f';//f - fulltime, e - extramural
     public char tuitionTerm = 'r';//r - regular, s - shortened
+    @JsonView(GroupViews.Name.class)
     public int studySemesters;
     public BigDecimal studyYears;
     public int beginYears;
     public boolean active;
+
 }
