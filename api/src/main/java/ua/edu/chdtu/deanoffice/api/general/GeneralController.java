e55a50b3cf950684cf3cfa4720fa21df86cc12e1
gzaspa <gzaspa@ukr.net>
2017-12-13 18:12:52 +0200

    implement /degrees API method

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/general/GeneralController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/GeneralController.java
@@ -1,20 +1,29 @@
 package ua.edu.chdtu.deanoffice.api.general;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ua.edu.chdtu.deanoffice.entity.Degree;
 import ua.edu.chdtu.deanoffice.service.GeneralService;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 @RestController
 public class GeneralController {
+    @Autowired
     private GeneralService generalService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/degrees")
     public ResponseEntity<List<NamedDTO>> getDegrees() {
-
-        return null;
+        List<Degree> degrees = generalService.getDegrees();
+        ModelMapper modelMapper = new ModelMapper();
+        Type listType = new TypeToken<List<NamedDTO>>() {}.getType();
+        List<NamedDTO> degreeDTOs = modelMapper.map(degrees, listType);
+        return ResponseEntity.ok(degreeDTOs);
     }
 }
