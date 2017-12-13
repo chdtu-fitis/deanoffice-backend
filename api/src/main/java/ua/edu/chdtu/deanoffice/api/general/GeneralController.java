0b3c337f241ca4b2099cdf927f428bad38748259
gzaspa <gzaspa@ukr.net>
2017-12-13 16:18:47 +0200

    create NamedDTO, add getDegrees to GeneralController

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/general/GeneralController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/GeneralController.java
@@ -1,7 +1,20 @@
 package ua.edu.chdtu.deanoffice.api.general;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import ua.edu.chdtu.deanoffice.service.GeneralService;
 
+import java.util.List;
+
+@RestController
 public class GeneralController {
     private GeneralService generalService;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/degrees")
+    public ResponseEntity<List<NamedDTO>> getDegrees() {
+
+        return null;
+    }
 }
---
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/NamedDTO.java
@@ -0,0 +1,11 @@
+package ua.edu.chdtu.deanoffice.api.general;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NamedDTO {
+    private int id;
+    private String name;
+}
