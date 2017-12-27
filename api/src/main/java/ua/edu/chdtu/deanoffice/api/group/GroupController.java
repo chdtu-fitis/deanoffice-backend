c97da4ee63c4ab3bb25b21793a2216d6eab027e5
gzaspa <gzaspa@ukr.net>
2017-12-27 15:04:47 +0200

    fix GroupController after merge

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/group/GroupController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/group/GroupController.java
@@ -1,9 +1,47 @@
 package ua.edu.chdtu.deanoffice.api.group;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ua.edu.chdtu.deanoffice.api.group.dto.CourseForGroupDTO;
+import ua.edu.chdtu.deanoffice.api.group.dto.GroupViews;
+import ua.edu.chdtu.deanoffice.api.group.dto.GroupDTO;
+import ua.edu.chdtu.deanoffice.entity.CourseForGroup;
+import ua.edu.chdtu.deanoffice.entity.StudentGroup;
+import ua.edu.chdtu.deanoffice.service.CourseForGroupService;
+import ua.edu.chdtu.deanoffice.service.GroupService;
+import java.lang.reflect.Type;
+import java.util.List;
 
 @RestController
-@RequestMapping("/group")
+@RequestMapping("/groups")
 public class GroupController {
+    @Autowired
+    private GroupService groupService;
+    @Autowired
+    private CourseForGroupService courseForGroupService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    @JsonView(GroupViews.Name.class)
+    public List<GroupDTO> getGroups() {
+        List<StudentGroup> studentGroups = groupService.getGroups();
+        Type listType = new TypeToken<List<GroupDTO>>() {}.getType();
+        ModelMapper modelMapper = new ModelMapper();
+        List<GroupDTO> groupDTOs = modelMapper.map(studentGroups,  listType);
+        return groupDTOs;
+    }
+
+    @RequestMapping("{id}/courses")
+    @ResponseBody
+    @JsonView(GroupViews.Name.class)
+    public List<CourseForGroupDTO> getCourses(@PathVariable String id) {
+        List<CourseForGroup> courseForGroups = courseForGroupService.getCourseForGroup(Integer.parseInt(id));
+        Type listType = new TypeToken<List<CourseForGroupDTO>>() {}.getType();
+        ModelMapper modelMapper = new ModelMapper();
+        List<CourseForGroupDTO> courseForGroupDTOS = modelMapper.map(courseForGroups,listType);
+        return  courseForGroupDTOS;
+    }
 }
