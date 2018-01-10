02d9b8136d832d9c065536d11bbffe0b2153ebd8
ValentynTulub <metryumora@gmail.com>
2018-01-10 10:03:55 +0200

    Moved templates to their own folder in resources, changed paths in code. Created document template for #13. Created service for #13.

---
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/document/report/gradepercentage/GradePercentageReportService.java
@@ -0,0 +1,10 @@
+package ua.edu.chdtu.deanoffice.service.document.report.gradepercentage;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GradePercentageReportService {
+
+
+
+}
