ff5f981af23c2170f34b9eb0a206788770316459
SashaKononenko <alex327021@gmail.com>
2018-02-02 16:39:11 +0200

    Fix GroupViews: delete interface Group

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/group/dto/GroupViews.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/group/dto/GroupViews.java
@@ -3,6 +3,5 @@ package ua.edu.chdtu.deanoffice.api.group.dto;
 public class GroupViews {
     public interface Name  {}
     public interface Course extends Name{}
-    public interface Group {}
 
 }
