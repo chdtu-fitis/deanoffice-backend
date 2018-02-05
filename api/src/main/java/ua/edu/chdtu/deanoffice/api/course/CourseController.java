51da2dcc30cbbfd844fac84f80f39907ee503512
SashaKononenko <alex327021@gmail.com>
2018-02-05 11:44:01 +0200

    Create CourseController

---
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/course/CourseController.java
@@ -0,0 +1,34 @@
+package ua.edu.chdtu.deanoffice.api.course;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import ua.edu.chdtu.deanoffice.api.group.dto.GroupDTO;
+import ua.edu.chdtu.deanoffice.api.group.dto.GroupViews;
+import ua.edu.chdtu.deanoffice.entity.StudentGroup;
+import ua.edu.chdtu.deanoffice.service.CourseService;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+    @RequestMapping("/{courseId}")
+    @ResponseBody
+    @JsonView(GroupViews.Name.class)
+    public List<GroupDTO> getGroupsByCourse(@PathVariable String courseId) {
+        List<StudentGroup> studentGroups = courseService.getGroupsByCourse(Integer.parseInt(courseId));
+        Type listType = new TypeToken<List<GroupDTO>>() {}.getType();
+        ModelMapper modelMapper = new ModelMapper();
+        List<GroupDTO> groupDTOs = modelMapper.map(studentGroups, listType);
+        return groupDTOs;
+    }
+}
