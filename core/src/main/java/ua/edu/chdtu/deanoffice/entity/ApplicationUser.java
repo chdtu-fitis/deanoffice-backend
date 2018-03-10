9d9e62f7b7a785f0b4528636b934e611576a2e8a
Serhii Shemshur <serhii.shemshur.dev@gmail.com>
2018-03-10 17:32:40 +0200

    Create ApplicationUser entity

---
+++ core/src/main/java/ua/edu/chdtu/deanoffice/entity/ApplicationUser.java
@@ -0,0 +1,22 @@
+package ua.edu.chdtu.deanoffice.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class ApplicationUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
