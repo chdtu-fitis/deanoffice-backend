b245013adf3c4f381816b05835cd7e2f16efd7c6
TarasTurchenko <TurchTas.1551@gmail.com>
2018-03-20 14:36:08 +0200

    Optimized imports in classes and added NamedView for NamedDTO

---
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/NamedView.java
@@ -0,0 +1,5 @@
+package ua.edu.chdtu.deanoffice.api.general;
+
+public class NamedView {
+    public interface Named {}
+}
--- api/src/main/java/ua/edu/chdtu/deanoffice/api/student/dto/StudentDegreeViews.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/student/dto/StudentDegreeViews.java
@@ -1,10 +1,12 @@
 package ua.edu.chdtu.deanoffice.api.student.dto;
 
+import static ua.edu.chdtu.deanoffice.api.general.NamedView.Named;
+
 public class StudentDegreeViews {
     public interface DetailAndDegree {}
     public interface SearchAndSimple {}
 
-    public interface Simple extends SearchAndSimple {}
+    public interface Simple extends SearchAndSimple, Named {}
     public interface Detail extends Simple, DetailAndDegree {}
     public interface Degree extends Simple, DetailAndDegree {}
     public interface Personal extends Detail {}
