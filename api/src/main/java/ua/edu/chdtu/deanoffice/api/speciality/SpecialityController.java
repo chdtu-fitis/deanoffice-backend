c09b71129adc9463a7c3693d682026f4150c7ac2
TarasTurchenko <TurchTas.1551@gmail.com>
2018-03-27 12:25:02 +0300

    Added controller for specialities, dto and view

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/speciality/SpecialityController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/speciality/SpecialityController.java
@@ -1,9 +1,32 @@
 package ua.edu.chdtu.deanoffice.api.speciality;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ua.edu.chdtu.deanoffice.api.speciality.dto.SpecialityDTO;
+import ua.edu.chdtu.deanoffice.entity.Speciality;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 @RestController
 @RequestMapping("/speciality")
 public class SpecialityController {
+
+    @GetMapping("")
+    public ResponseEntity getSpecialities(
+            @RequestParam(value = "only-active", required = false, defaultValue = "true") boolean onlyAcyive
+    ) {
+        List<Speciality> specialities;
+        return ResponseEntity.ok(parseToSpecialityDTO(specialities));
+    }
+
+    private List<SpecialityDTO> parseToSpecialityDTO(List<Speciality> specialities) {
+        Type type = new TypeToken<List<SpecialityDTO>>() {}.getType();
+        return new ModelMapper().map(specialities, type);
+    }
 }
---
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/speciality/dto/SpecialityDTO.java
@@ -0,0 +1,19 @@
+package ua.edu.chdtu.deanoffice.api.speciality.dto;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SpecialityDTO {
+    @JsonView(SpecialityView.Basic.class)
+    private int id;
+    @JsonView(SpecialityView.Basic.class)
+    private String name;
+    private String nameEng;
+    private boolean active;
+    private String code;
+    private String fieldOfStudy;
+    private String fieldOfStudyEng;
+}
