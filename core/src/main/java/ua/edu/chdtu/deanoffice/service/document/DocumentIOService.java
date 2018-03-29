7a3be54914a0f1c4757dfdb1d689fb8ee9efab86
Yevhenii Riabchych <riabchych@gmail.com>
2018-03-29 20:18:39 +0300

    Added methods for loading XLSX documents Updated method saveDocumentToTemp

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/document/DocumentIOService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/document/DocumentIOService.java
@@ -1,6 +1,7 @@
 package ua.edu.chdtu.deanoffice.service.document;
 
 import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.SpreadsheetMLPackage;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class DocumentIOService {
@@ -16,10 +18,31 @@ public class DocumentIOService {
         return WordprocessingMLPackage.load(new FileInputStream(new ClassPathResource(name).getFile()));
     }
 
-    public File saveDocumentToTemp(WordprocessingMLPackage template, String fileName) throws Docx4JException {
+    public SpreadsheetMLPackage loadSpreadsheetDocument(String name) throws IOException, Docx4JException {
+        return SpreadsheetMLPackage.load(new FileInputStream(new ClassPathResource(name).getFile()));
+    }
+
+    public SpreadsheetMLPackage loadSpreadsheetDocument(InputStream xlsxInputStream) throws Docx4JException {
+        return SpreadsheetMLPackage.load(xlsxInputStream);
+    }
+
+    public File saveDocumentToTemp(WordprocessingMLPackage document, String fileName) throws Docx4JException {
+        return saveDocument(document, fileName);
+    }
+
+    public File saveDocumentToTemp(SpreadsheetMLPackage document, String fileName) throws Docx4JException {
+        return saveDocument(document, fileName);
+    }
+
+    private File saveDocument(Object document, String fileName) throws Docx4JException {
         String finalFileName = cleanFileName(fileName);
         File documentFile = new File(System.getProperty("java.io.tmpdir") + finalFileName);
-        template.save(documentFile);
+
+        (document instanceof WordprocessingMLPackage
+                ? ((WordprocessingMLPackage) document)
+                : ((SpreadsheetMLPackage) document)
+        ).save(documentFile);
+
         return documentFile;
     }
 
