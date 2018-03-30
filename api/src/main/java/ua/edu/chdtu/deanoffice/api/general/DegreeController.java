94dc4dfee3cefe5e05862d72497029f94b73012e
TarasTurchenko <TurchTas.1551@gmail.com>
2018-03-30 13:51:55 +0300

    Refactor: getDegrees in DegreeController

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/general/DegreeController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/DegreeController.java
@@ -1,31 +1,30 @@
 package ua.edu.chdtu.deanoffice.api.general;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import ua.edu.chdtu.deanoffice.api.general.dto.NamedDTO;
 import ua.edu.chdtu.deanoffice.entity.Degree;
 import ua.edu.chdtu.deanoffice.service.DegreeService;
 
-import java.lang.reflect.Type;
 import java.util.List;
+
+import static ua.edu.chdtu.deanoffice.api.general.Util.parseToNanedDTO;
 
 @RestController
 @RequestMapping("/degrees")
 public class DegreeController {
-    @Autowired
-    private DegreeService degreeService;
+    private final DegreeService degreeService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<NamedDTO>> getDegrees() {
+    @Autowired
+    public DegreeController(DegreeService degreeService) {
+        this.degreeService = degreeService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity getDegrees() {
         List<Degree> degrees = degreeService.getDegrees();
-        ModelMapper modelMapper = new ModelMapper();
-        Type listType = new TypeToken<List<NamedDTO>>() {}.getType();
-        List<NamedDTO> degreeDTOs = modelMapper.map(degrees, listType);
-        return ResponseEntity.ok(degreeDTOs);
+        return ResponseEntity.ok(parseToNanedDTO(degrees));
     }
 }
--- api/src/main/java/ua/edu/chdtu/deanoffice/api/general/Util.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/Util.java
@@ -1,7 +1,13 @@
 package ua.edu.chdtu.deanoffice.api.general;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import ua.edu.chdtu.deanoffice.api.general.dto.NamedDTO;
+
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
@@ -15,5 +21,11 @@ public class Util {
     public static URI getNewResourceLocation(Object[] ids) {
         String id = Arrays.stream(ids).map(Object::toString).collect(Collectors.joining(","));
         return fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+    }
+
+
+    static List<NamedDTO> parseToNanedDTO(List data) {
+        Type listType = new TypeToken<List<NamedDTO>>() {}.getType();
+        return new ModelMapper().map(data, listType);
     }
 }
