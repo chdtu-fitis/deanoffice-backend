7c633f06c4ebcdb7a0a69bfec8fa10d5d8ab4f95
Serhii Shemshur <serhii.shemshur.dev@gmail.com>
2018-04-01 18:04:06 +0300

    Update application user migration: add faculty_id

--- core/src/main/resources/db/migration/V6__create_application_user.sql
+++ core/src/main/resources/db/migration/V6__create_application_user.sql
@@ -4,7 +4,10 @@ create table application_user
 	first_name varchar(255) not null,
 	last_name varchar(2555) not null,
 	password varchar(255) not null,
-	username varchar(255)
+	username varchar(255),
+	faculty_id integer
+		constraint application_user_faculty_id_fk
+			references faculty
 )
 ;
 
@@ -12,4 +15,8 @@ create unique index application_user_id_uindex
 	on application_user (id)
 ;
 
-INSERT INTO application_user (id, first_name, last_name, password, username) VALUES (3, 'admin', 'admin', 'test', 'test');
+create unique index application_user_username_uindex
+	on application_user (username)
+;
+
+INSERT INTO public.application_user (id, first_name, last_name, password, username, faculty_id) VALUES (3, 'admin', 'admin', 'test', 'test', 1);
