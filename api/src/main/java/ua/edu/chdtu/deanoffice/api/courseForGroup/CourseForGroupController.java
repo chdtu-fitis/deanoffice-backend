010358355954c2d2077594fdfbf4adc1acd545bc
gzaspa <gzaspa@ukr.net>
2018-04-03 13:00:35 +0300

    fix: DTO in CourseForGroup controller

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/courseForGroup/CourseForGroupController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/courseForGroup/CourseForGroupController.java
@@ -1,16 +1,10 @@
 package ua.edu.chdtu.deanoffice.api.courseForGroup;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ua.edu.chdtu.deanoffice.api.group.dto.CourseForGroupDTO;
 import ua.edu.chdtu.deanoffice.entity.CourseForGroup;
 import ua.edu.chdtu.deanoffice.service.CourseForGroupService;
-
-import java.lang.reflect.Type;
-import java.util.HashSet;
 import java.util.List;
 
 @RestController
@@ -19,7 +13,7 @@ public class CourseForGroupController {
     @Autowired
     private CourseForGroupService courseForGroupService;
 
-    @PostMapping(value = "/{studentGroupId}/coursesForGroup")
+    @PostMapping(value = "/{studentGroupId}")
     public ResponseEntity.BodyBuilder addCoursesForGroup(@RequestBody List<CourseForGroup> newCourses, @RequestBody List<CourseForGroup> mutableCourses, @RequestBody List<String> deleteCoursesIdList, @PathVariable String studentGroupId) {
         if (newCourses != null || mutableCourses !=null || deleteCoursesIdList != null){
             courseForGroupService.addCourseForGroupAndNewChanges(newCourses, mutableCourses, deleteCoursesIdList, Integer.parseInt(studentGroupId));
