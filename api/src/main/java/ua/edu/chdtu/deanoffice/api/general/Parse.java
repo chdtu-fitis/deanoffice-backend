bda37e2255c820b60c54c90c013d602b82ae84de
TarasTurchenko <TurchTas.1551@gmail.com>
2018-04-04 02:54:27 +0300

    Created Created method toList

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/general/Parse.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/Parse.java
@@ -2,8 +2,17 @@ package ua.edu.chdtu.deanoffice.api.general;
 
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Parse {
     public static Object toObject(Object source, Class type) {
         return new ModelMapper().map(source, type);
+    }
+
+
+    public static List toList(List source, Class type) {
+        ModelMapper modelMapper = new ModelMapper();
+        return (List) source.stream().map(el -> modelMapper.map(el, type)).collect(Collectors.toList());
     }
 }
--- api/src/test/java/ua/edu/chdtu/deanoffice/api/general/ParseTest.java
+++ api/src/test/java/ua/edu/chdtu/deanoffice/api/general/ParseTest.java
@@ -1,31 +1,46 @@
 package ua.edu.chdtu.deanoffice.api.general;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 import ua.edu.chdtu.deanoffice.api.student.dto.StudentDTO;
 import ua.edu.chdtu.deanoffice.entity.Student;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
-import static ua.edu.chdtu.deanoffice.api.general.Parse.toObject;
 
 public class ParseTest {
     private StudentDTO sourceObject = new StudentDTO();
     private Student expectedObject = new Student();
+    private List<StudentDTO> sourceList = new ArrayList<>();
+    private List<Student> expectedList = new ArrayList<>();
 
-    @Before
-    public void setUp() {
-        setUpObjects();
-    }
-
-    private void setUpObjects() {
+    public ParseTest() {
         sourceObject.setEmail("example@com");
         expectedObject.setEmail("example@com");
     }
 
     @Test
     public void object() {
-        Student actualObject = (Student) toObject(sourceObject, Student.class);
+        Student actualObject = (Student) Parse.toObject(sourceObject, Student.class);
         String actualEmail = actualObject.getEmail();
         assertEquals(actualEmail, expectedObject.getEmail());
+    }
+
+    @Before
+    public void setUpList() {
+        sourceList.add(sourceObject);
+        expectedList.add(expectedObject);
+    }
+
+    @Test
+    public void list() {
+        List<Student> actualList = Parse.toList(sourceList, Student.class);
+        String actualEmail = actualList.get(0).getEmail();
+        String expectedEmail = expectedList.get(0).getEmail();
+        assertEquals(actualEmail, expectedEmail);
     }
 }
