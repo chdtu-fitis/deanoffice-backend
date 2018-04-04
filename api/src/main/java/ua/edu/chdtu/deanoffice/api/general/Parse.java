f5c219d04c115f45ceb444236e697fe9b05211df
TarasTurchenko <TurchTas.1551@gmail.com>
2018-04-04 11:37:36 +0300

    Fixed parser for List

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/general/Parse.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/Parse.java
@@ -2,8 +2,9 @@ package ua.edu.chdtu.deanoffice.api.general;
 
 import org.modelmapper.ModelMapper;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Parse {
     public static Object toObject(Object source, Class type) {
@@ -12,7 +13,30 @@ public class Parse {
 
 
     public static List toList(List source, Class type) {
-        ModelMapper modelMapper = new ModelMapper();
-        return (List) source.stream().map(el -> modelMapper.map(el, type)).collect(Collectors.toList());
+        return new ModelMapper().map(source, new ListParameterizedType(type));
+    }
+}
+
+class ListParameterizedType implements ParameterizedType {
+
+    private Type type;
+
+    ListParameterizedType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public Type[] getActualTypeArguments() {
+        return new Type[] {type};
+    }
+
+    @Override
+    public Type getRawType() {
+        return List.class;
+    }
+
+    @Override
+    public Type getOwnerType() {
+        return null;
     }
 }
