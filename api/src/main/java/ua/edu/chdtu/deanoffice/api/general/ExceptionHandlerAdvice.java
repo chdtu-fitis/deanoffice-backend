08048a91f0ce1ea13abd5ff398d4462959f8bc70
TarasTurchenko <TurchTas.1551@gmail.com>
2018-04-06 19:55:15 +0300

    Handle exception with custom message

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/general/ExceptionHandlerAdvice.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/ExceptionHandlerAdvice.java
@@ -10,18 +10,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
 
-    private static final HttpStatus defaultResponseStatus = HttpStatus.UNPROCESSABLE_ENTITY;
+    private static final HttpStatus DEFAULT_RESPONSE_STATUS = HttpStatus.UNPROCESSABLE_ENTITY;
     private static Logger logger = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
 
     @ExceptionHandler(Exception.class)
-    public static ResponseEntity handleException(Exception exception, HttpStatus responseStatus) {
-        logger.error(exception.getMessage());
-        return ResponseEntity
-                .status(responseStatus)
-                .body(exception.getMessage());
+    public static ResponseEntity handleException(String message, HttpStatus responseStatus) {
+        logger.error(message);
+        return ResponseEntity.status(responseStatus).body(message);
     }
 
     public static ResponseEntity handleException(Exception exception) {
-        return handleException(exception, defaultResponseStatus);
+        return handleException(exception.getMessage(), DEFAULT_RESPONSE_STATUS);
+    }
+
+    public static ResponseEntity handleException(String message) {
+        return handleException(message, DEFAULT_RESPONSE_STATUS);
     }
 }
