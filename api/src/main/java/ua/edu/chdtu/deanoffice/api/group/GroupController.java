dc493a689bfc31b323dba10d403585163835a242
TarasTurchenko <TurchTas.1551@gmail.com>
2018-05-02 14:26:12 +0300

    Fixed handle exception in GroupController. Add Jacadoc to ExceptionHandlerAdvice

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/group/GroupController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/group/GroupController.java
@@ -2,13 +2,11 @@ package ua.edu.chdtu.deanoffice.api.group;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ua.edu.chdtu.deanoffice.api.general.ExceptionHandlerAdvice;
 import ua.edu.chdtu.deanoffice.api.general.dto.NamedDTO;
 import ua.edu.chdtu.deanoffice.api.group.dto.StudentGroupDTO;
 import ua.edu.chdtu.deanoffice.api.group.dto.StudentGroupShortDTO;
@@ -18,7 +16,6 @@ import ua.edu.chdtu.deanoffice.entity.StudentGroup;
 import ua.edu.chdtu.deanoffice.service.StudentGroupService;
 import ua.edu.chdtu.deanoffice.webstarter.security.CurrentUser;
 
-import java.security.Principal;
 import java.util.List;
 
 import static ua.edu.chdtu.deanoffice.api.general.mapper.Mapper.map;
@@ -61,12 +58,7 @@ public class GroupController {
             @RequestParam(value = "only-active", required = false, defaultValue = "true") boolean onlyActive,
             @CurrentUser ApplicationUser applicationUser
     ) {
-        if (applicationUser != null){
-            List<StudentGroup> studentGroups = studentGroupService.getAllByActive(onlyActive, applicationUser.getFaculty().getId());
-            return ResponseEntity.ok(parse(studentGroups, StudentGroupDTO.class));
-        }
-        else {
-            return ExceptionHandlerAdvice.handleException("Not authorized", NullPointerException.class, HttpStatus.UNAUTHORIZED);
-        }
+        List<StudentGroup> studentGroups = studentGroupService.getAllByActive(onlyActive, applicationUser.getFaculty().getId());
+        return ResponseEntity.ok(map(studentGroups, StudentGroupDTO.class));
     }
 }
