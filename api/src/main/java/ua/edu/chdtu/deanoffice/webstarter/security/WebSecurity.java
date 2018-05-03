df4b2660f8499988b9b6d11a7f195a991ef4f446
gzaspa <gzaspa@ukr.net>
2018-05-03 13:53:30 +0300

    fix: change CORS configuration for production server

--- api/src/main/java/ua/edu/chdtu/deanoffice/webstarter/security/WebSecurity.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/webstarter/security/WebSecurity.java
@@ -36,7 +36,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://is.chdtu.edu.ua:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
