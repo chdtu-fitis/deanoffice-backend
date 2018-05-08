4e8c254af0a7fceda1465945e3630d9d2979c5c4
TarasTurchenko <TurchTas.1551@gmail.com>
2018-05-08 16:56:57 +0300

    Created controller for department. Get all departments by id

--- core/src/main/java/ua/edu/chdtu/deanoffice/repository/DepartmentRepository.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/repository/DepartmentRepository.java
@@ -1,7 +1,16 @@
 package ua.edu.chdtu.deanoffice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.edu.chdtu.deanoffice.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+
+    @Query("select d from Department d " +
+            "where d.faculty.id = :faculty_id " +
+            "order by d.name")
+    List<Department> getAllByFaculty(@Param("faculty_id") int facultyId);
 }
--- core/src/main/java/ua/edu/chdtu/deanoffice/service/DepartmentService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/DepartmentService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import ua.edu.chdtu.deanoffice.entity.Department;
 import ua.edu.chdtu.deanoffice.repository.DepartmentRepository;
 
+import java.util.List;
+
 @Service
 public class DepartmentService {
     private final DepartmentRepository departmentRepository;
@@ -17,5 +19,9 @@ public class DepartmentService {
 
     public Department getById(Integer departmentId) {
         return departmentRepository.findOne(departmentId);
+    }
+
+    public List<Department> getAll(int facultyId) {
+        return departmentRepository.getAllByFaculty(facultyId);
     }
 }
