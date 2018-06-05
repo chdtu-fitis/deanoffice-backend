592088c3b8e38e8156a510cd07ba258d6564684c
KoctrX <kostja071@gmail.com>
2018-06-05 19:11:27 +0300

    fix imports

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/grade/GradeController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/grade/GradeController.java
@@ -2,7 +2,14 @@ package ua.edu.chdtu.deanoffice.api.grade;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionHandlerAdvice;
 import ua.edu.chdtu.deanoffice.api.grade.dto.GradeDTO;
 import ua.edu.chdtu.deanoffice.entity.CourseForGroup;
