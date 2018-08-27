f3ca69b1cdb827e609c1394103518e8d5b8fa1a9
gzaspa <gzaspa@ukr.net>
2018-08-27 22:03:53 +0300

    add: exclude showing stack trace for non-reportable exceptions

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/general/ExceptionHandlerAdvice.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/general/ExceptionHandlerAdvice.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import ua.edu.chdtu.deanoffice.exception.PageNotFoundException;
+import ua.edu.chdtu.deanoffice.exception.UnauthorizedFacultyDataException;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -33,11 +35,19 @@ public class ExceptionHandlerAdvice {
     }
 
     public static ResponseEntity handleException(Exception exception, Class exceptionLocation, HttpStatus responseStatus) {
-        getLogger(exceptionLocation).error("ERROR", exception);
+        if (isReportableException(exception))
+            getLogger(exceptionLocation).error("ERROR", exception);
         return ResponseEntity.status(responseStatus).body(exception.getMessage());
     }
 
     public static ResponseEntity handleException(String message, Class exceptionLocation) {
         return handleException(message, exceptionLocation, DEFAULT_RESPONSE_STATUS);
+    }
+
+    private static boolean isReportableException(Exception e) {
+        if (e instanceof PageNotFoundException || e instanceof UnauthorizedFacultyDataException)
+            return false;
+        else
+            return true;
     }
 }
--- core/src/main/resources/db/migration/V9__delete-degree_id-in-student_degree.sql
+++
@@ -1,5 +0,0 @@
--- Deleting degree_id in student_degree
-
-alter table student_degree
-  drop constraint fk9st6a1j5cw6s3xkakvnavyi99,
-  drop column degree_id
