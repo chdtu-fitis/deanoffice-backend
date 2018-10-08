bbe4eb0981b0a3147137d93d9cf9d3c74f2eb4be
NicolasFaydik <787898faydikripper>
2018-10-08 11:23:01 +0300

    develop: student edebo sync: controller returns orange list as json

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/student/synchronization/edebo/SyncronizationController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/student/synchronization/edebo/SyncronizationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionHandlerAdvice;
+import ua.edu.chdtu.deanoffice.api.general.dto.NamedDTO;
 import ua.edu.chdtu.deanoffice.api.general.mapper.Mapper;
 import ua.edu.chdtu.deanoffice.api.speciality.dto.SpecialityView;
 import ua.edu.chdtu.deanoffice.api.student.dto.StudentDTO;
@@ -30,8 +31,6 @@ public class SyncronizationController {
         this.edeboDataSynchronizationService = edeboDataSynchronizationService;
     }
 
-    //@JsonView(StudentView.Degree.class)
-    @JsonView(SpecialityView.Basic.class)
     @PostMapping("/edebo-synchronization")
     public ResponseEntity studentsEdeboSynchronization(@RequestParam("file") MultipartFile uploadfile) {
         if (uploadfile.isEmpty()) {
@@ -42,9 +41,8 @@ public class SyncronizationController {
 
         try {
             edeboDataSynchronizationReport = edeboDataSynchronizationService.getEdeboDataSynchronizationReport(uploadfile.getInputStream());
-            List<StudentDegreePrimaryEdeboDataDTO> noSuchStudentDegreeInDbOrangeDTOs = Mapper.map(edeboDataSynchronizationReport.getNoSuchStudentOrSuchStudentDegreeInDbOrange(), StudentDegreeFullEdeboDataDto.class);
+            List<StudentDegreeFullEdeboDataDto> noSuchStudentDegreeInDbOrangeDTOs = Mapper.map(edeboDataSynchronizationReport.getNoSuchStudentOrSuchStudentDegreeInDbOrange(), StudentDegreeFullEdeboDataDto.class);
             return ResponseEntity.ok(noSuchStudentDegreeInDbOrangeDTOs);
-//            importDataService.saveImport(edeboDataSyncronizationReport);
         } catch (Exception exception) {
             return handleException(exception);
         }
