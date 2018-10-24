0314b3da4876699ee9f0c6602a4542db47e25156
volodymyr-polishchuk <thevivalley@gmail.com>
2018-10-24 12:10:02 +0300

    refactor: add support StudentPreviousUniversity in StudentDegree

--- core/src/main/java/ua/edu/chdtu/deanoffice/entity/StudentDegree.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/entity/StudentDegree.java
@@ -3,14 +3,11 @@ package ua.edu.chdtu.deanoffice.entity;
 import lombok.Getter;
 import lombok.Setter;
 import ua.edu.chdtu.deanoffice.entity.superclasses.BaseEntity;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+
+import javax.persistence.*;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -55,4 +52,7 @@ public class StudentDegree extends BaseEntity {
 
     @Temporal(TemporalType.DATE)
     private Date admissionDate;
+
+    @OneToMany(mappedBy = "studentDegree", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<StudentPreviousUniversity> studentPreviousUniversities;
 }
