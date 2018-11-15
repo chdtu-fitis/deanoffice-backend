ed00b1ad90a6ef1dff2f01fdda844d4e1e819abb
Vlad Muzychenko <pexterexpert@gmail.com>
2018-11-15 20:26:26 +0200

    add: thesis import: add importing thesis data from the file, formed method for thesis import report

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/thesis/ThesisImportService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/thesis/ThesisImportService.java
@@ -1,15 +1,21 @@
 package ua.edu.chdtu.deanoffice.service.datasync.thesis;
 
+import org.docx4j.TraversalUtil;
+import org.docx4j.finders.TableFinder;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.edu.chdtu.deanoffice.service.document.DocumentIOService;
 
+import javax.xml.bind.JAXBElement;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ThesisImportService {
@@ -25,10 +31,23 @@ public class ThesisImportService {
         if (docxInputStream == null){
             throw new Exception("Помилка часу виконання");
         }
-        List<ThesisImportData> thesisImportData = getThesisesFromStream(docxInputStream);
-        //ThesisImportService thesisImportService = null;
-        //thesisImportService = getThesisImportService(docxInputStream);
-        return null;
+        try{
+            List<ThesisImportData> thesisImportData = getThesisesFromStream(docxInputStream);
+            Objects.requireNonNull(thesisImportData);
+            ThesisReport thesisReport = new ThesisReport();
+            for (ThesisImportData thesisData: thesisImportData){
+                addSynchronizationReportForThesisImportedData(thesisData);
+            }
+            return null;
+        } catch (Docx4JException e){
+            e.printStackTrace();
+            throw new Exception("Помилка обробки файлу");
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new Exception("Помилка читання файлу");
+        } finally {
+            docxInputStream.close();
+        }
     }
 
     private List<ThesisImportData> getThesisesFromStream(InputStream docxInputStream) throws Docx4JException, IOException {
@@ -45,19 +64,100 @@ public class ThesisImportService {
     }
 
     private List<ThesisImportData> getThesisImportedDataFromDocxPkg(WordprocessingMLPackage docxPkg) {
+        MainDocumentPart mainDocumentPart = docxPkg.getMainDocumentPart();
+        List<ThesisImportData> thesisImportDatas = new ArrayList();
 
-        List<ThesisImportData> thesisImportData = new ArrayList();
-        return thesisImportData;
+        TableFinder finder = new TableFinder();
+        new TraversalUtil(mainDocumentPart.getContent(), finder);
+        int rowCount = 0;
+        int cellCount = 1;
+
+        for (Object table: finder.tblList){
+            Tbl tbl = (Tbl) table;
+            String groupName = "";
+            List<Object> allRows = tbl.getContent();
+
+            for (Object tableRow: allRows){
+                if (rowCount == 1){
+                    rowCount++;
+                    continue;
+                }
+                ThesisImportData thesisImportData = new ThesisImportData();
+                Tr row = (Tr) tableRow;
+                List<Object> allCells = row.getContent();
+                String cellData = "";
+                cellCount = 1;
+
+                for (Object tableCell: allCells){
+                    if (cellCount == 1){
+                        cellCount++;
+                        continue;
+                    }
+                    if (!cellData.equals("")){
+                        cellData += "/";
+                    }
+                    Tc cell = null;
+                    if (tableCell instanceof JAXBElement) {
+                        JAXBElement jaxbElement = (JAXBElement) tableCell;
+                        cell = (Tc) jaxbElement.getValue();
+                    } else
+                        cell = (Tc) tableCell;
+                    List<Object> allPara = cell.getContent();
+
+                    for (Object tableParagraph: allPara){
+                        P paragraph = (P) tableParagraph;
+                        if (paragraph.getRsidRPr().equals("00DE0929")){
+                            continue;
+                        }
+                        List<Object> allRun = paragraph.getContent();
+                        if (allRun.size() == 0){
+                            continue;
+                        }
+
+                        for (Object tableRun: allRun){
+                            if (!(tableRun instanceof R)){
+                                continue;
+                            }
+                            R run = (R) tableRun;
+                            List<Object> allText = run.getContent();
+
+                            for (Object tableText: allText){
+                                Text text = null;
+                                if (tableText instanceof JAXBElement){
+                                    JAXBElement jaxbElement = (JAXBElement) tableText;
+                                    text = (Text) jaxbElement.getValue();
+                                } else {
+                                    text = (Text) tableText;
+                                }
+                                if (rowCount == 0){
+                                    rowCount = 1;
+                                    groupName = text.getValue();
+                                } else
+                                    cellData += text.getValue();
+                            }
+                        }
+                    }
+                }
+                if(!cellData.equals("")){
+                    String rowParts[] = cellData.split("/", 4);
+                    String[] studentData = rowParts[0].split(" ", 3);
+                    thesisImportData.setLastName(studentData[0]);
+                    thesisImportData.setFirstName(studentData[1]);
+                    thesisImportData.setMiddleName(studentData[2]);
+                    thesisImportData.setThesisName(rowParts[1]);
+                    thesisImportData.setThesisNameEng(rowParts[2]);
+                    thesisImportData.setFullSupervisorName(rowParts[3]);
+                    thesisImportData.setGroupName(groupName);
+                    thesisImportDatas.add(thesisImportData);
+                    cellData = "";
+                }
+            }
+            rowCount = 0;
+        }
+        return thesisImportDatas;
     }
 
+    public void addSynchronizationReportForThesisImportedData(ThesisImportData thesisImportData){
+
+    }
 }
-
-
-//    MainDocumentPart mainDocumentPart = docxPkg.getMainDocumentPart();
-//        String textNotesPath = "//w:t";
-//        List <Object> textNotes = mainDocumentPart.getJAXBNodesViaXPath(textNotesPath, true);
-//        for (Object obj : textNotes) {
-//            Text text = (Text) ((JAXBElement) obj).getValue();
-//            String textValue = text.getValue();
-//            System.out.println(textValue);
-//        }
