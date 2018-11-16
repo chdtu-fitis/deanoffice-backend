a7caeec3a72abe9409748e9f983a614b4641986f
Alex <alex222438@gmail.com>
2018-11-16 19:22:18 +0200

    1. The bold letter "k" is placed near the contract students 2. the file name was created with the course and date of creation

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/document/report/journal/GradesJournalService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/document/report/journal/GradesJournalService.java
@@ -7,7 +7,9 @@ import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ua.edu.chdtu.deanoffice.entity.Payment;
 import ua.edu.chdtu.deanoffice.entity.Student;
+import ua.edu.chdtu.deanoffice.entity.StudentDegree;
 import ua.edu.chdtu.deanoffice.entity.StudentGroup;
 import ua.edu.chdtu.deanoffice.service.StudentGroupService;
 
@@ -15,6 +17,8 @@ import org.springframework.core.io.Resource;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -36,7 +40,8 @@ public class GradesJournalService {
         List<StudentGroup> studentGroups = studentGroupService.getGroupsByDegreeAndYear(degreeId, year, facultyId);
         if (studentGroups != null && studentGroups.size() != 0) {
             Document document = new Document(PageSize.A4, 28f, 28f, 28f, 28f);
-            String filePath = getJavaTempDirectory() + "/" + "StudentGroupsList.pdf";
+            String filePath = getJavaTempDirectory() + "/" + "StudentGroupsList_" + year +
+                    "course_" + getFileCreationDateAndTime() + ".pdf";
             File file = new File(filePath);
             PdfWriter.getInstance(document, new FileOutputStream(file));
             BaseFont baseFont = BaseFont.createFont(ttf.getURI().getPath(), BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
@@ -49,7 +54,7 @@ public class GradesJournalService {
         return null;
     }
 
-    private PdfPTable addContent(List<StudentGroup> studentGroups, Font font) throws DocumentException {
+    private PdfPTable addContent(List<StudentGroup> studentGroups, Font font) throws DocumentException, IOException {
         PdfPTable tableMain = new PdfPTable(2);
         tableMain.setLockedWidth(true);
         tableMain.setTotalWidth(425);
@@ -65,8 +70,9 @@ public class GradesJournalService {
         boolean oneOrTwo = true;
         int sumOfCellsInTheTable1 = 0;
         int sumOfCellsInTheTable2 = 0;
-        for (StudentGroup studentGroup : studentGroups) {
-            List<Student> students = studentGroup.getActiveStudents();
+        for (StudentGroup studentGroup : studentGroups)  {
+            List<StudentDegree> studentDegrees = studentGroup.getStudentDegrees();
+            //List<Student> students = studentGroup.getActiveStudents();
             PdfPCell groupNameCell = new PdfPCell(new Phrase(studentGroup.getName(), font));
             groupNameCell.setFixedHeight(28);//setMinimumHeight(30);
             groupNameCell.setPadding(5);
@@ -76,8 +82,14 @@ public class GradesJournalService {
             } else {
                 sumOfCellsInTheTable2 = addCellToTable(table2, groupNameCell, sumOfCellsInTheTable2);
             }
-            for (Student student : students){
-                PdfPCell studentCell = new PdfPCell(new Phrase(student.getSurname() + " " + student.getName() + " " + student.getPatronimic(), font));
+            for (StudentDegree studentDegree : studentDegrees){
+                Phrase studentText = new Phrase(studentDegree.getStudent().getSurname() + " "
+                        + studentDegree.getStudent().getName() + " "
+                        + studentDegree.getStudent().getPatronimic(), font);
+                if (studentDegree.getPayment() == Payment.CONTRACT){
+                    studentText.add(new Chunk(" K", new Font(Font.FontFamily.TIMES_ROMAN, 10, Font.BOLDITALIC)));
+                }
+                PdfPCell studentCell = new PdfPCell(studentText);
                 studentCell.setFixedHeight(28);//setMinimumHeight(30);
                 if (oneOrTwo){
                     sumOfCellsInTheTable1 = addCellToTable(table1, studentCell, sumOfCellsInTheTable1);
@@ -104,5 +116,9 @@ public class GradesJournalService {
     private int addCellToTable(PdfPTable table, PdfPCell cell, int sumOfCellsInTheTable) throws DocumentException{
             table.addCell(cell);
             return ++sumOfCellsInTheTable;
+    }
+
+    private String getFileCreationDateAndTime() {
+        return new SimpleDateFormat(" dd-MM-yyyy HH-mm").format(new Date());
     }
 }
