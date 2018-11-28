c6a232417a7a068d1497b1ec377495dcd4a64778
Vlad <pexterexpert@gmail.com>
2018-11-28 09:54:21 +0200

    fix: thesis import: changed location of ThesisController and thesis DTOs classes

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/student/synchronization/thesis/dto/ImportedThesisDataDTO.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/student/synchronization/thesis/dto/ImportedThesisDataDTO.java
@@ -1,4 +1,4 @@
-package ua.edu.chdtu.deanoffice.api.student.synchronization.thesis;
+package ua.edu.chdtu.deanoffice.api.student.synchronization.thesis.dto;
 
 import lombok.Getter;
 import lombok.Setter;
--- api/src/main/java/ua/edu/chdtu/deanoffice/api/student/synchronization/thesis/dto/MissingThesisDataRedDTO.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/student/synchronization/thesis/dto/MissingThesisDataRedDTO.java
@@ -1,8 +1,7 @@
-package ua.edu.chdtu.deanoffice.api.student.synchronization.thesis;
+package ua.edu.chdtu.deanoffice.api.student.synchronization.thesis.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import ua.edu.chdtu.deanoffice.api.student.synchronization.thesis.ImportedThesisDataDTO;
 
 @Getter
 @Setter
