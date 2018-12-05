466fa2c54a0387d1f49aea3a5a135d81a6f1a18b
Volodymyr Polishchuk <thevivalley@gmail.com>
2018-12-05 14:53:06 +0200

    add: method to repository and service, implemented student filter (#143)

--- core/src/main/java/ua/edu/chdtu/deanoffice/repository/GradeRepository.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/repository/GradeRepository.java
@@ -49,6 +49,29 @@ public interface GradeRepository extends JpaRepository<Grade, Integer> {
     @Query("select gr from Grade gr where gr.course.id = :courseId and gr.studentDegree.studentGroup.id = :groupId")
     List<Grade> findByCourseAndGroup(@Param("courseId") int courseId, @Param("groupId") int groupId);
 
+    @Query(value =
+            "SELECT CAST(CAST(COUNT(*) AS INT) AS BOOLEAN) AS is_good_mark" +
+            "  FROM student_degree" +
+            "        LEFT JOIN grade ON student_degree.id = grade.student_degree_id" +
+            " WHERE student_degree.id = :studentDegreeId " +
+            "       AND student_degree.student_group_id = :studentGroupId " +
+            "       AND grade.points >= 60 " +
+            "       AND course_id IN (SELECT similar_course.id " +
+            "                           FROM course similar_course " +
+            "                          WHERE course_name_id IN (SELECT course_name.course_name_id " +
+            "                                                     FROM course course_name " +
+            "                                                    WHERE course_name.id = :courseId) " +
+            "                                AND kc_id IN (SELECT course_kc.kc_id " +
+            "                                                FROM course course_kc " +
+            "                                               WHERE course_kc.id = :courseId) " +
+            "                                AND hours IN (SELECT course_hours.hours " +
+            "                                                FROM course course_hours " +
+            "                                               WHERE course_hours.id = :courseId)) ",
+            nativeQuery = true)
+    boolean isStudentHaveGoodMarkFromCourse(@Param("studentDegreeId") Integer studentDegreeId,
+                                            @Param("studentGroupId") Integer studentGroupId,
+                                            @Param("courseId") Integer courseId);
+
     @Modifying
     @Query("update Grade as g set g.course.id = :newId " +
             "where g.course.id = :oldId")
