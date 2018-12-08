7ebb5babe714a1e71bff65c6d5601e8165c9f777
Vlad Muzychenko <pexterexpert@gmail.com>
2018-12-08 11:48:20 +0200

    add: diploma numb synch: add all header patterns to DiplomaHeaderPatterns

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/diploma/number/DiplomaHeaderPatterns.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/diploma/number/DiplomaHeaderPatterns.java
@@ -1,4 +1,16 @@
 package ua.edu.chdtu.deanoffice.service.datasync.edebo.diploma.number;
 
-public class DiplomaHeaderPatterns {
+final class DiplomaHeaderPatterns {
+    final static String FIRST_NAME = "firstName";
+    final static String LAST_NAME = "lastName";
+    final static String MIDDLE_NAME = "middleName";
+    final static String FIRST_NAME_EN = "firstNameEn";
+    final static String LAST_NAME_EN = "lastNameEn";
+    final static String MIDDLE_NAME_EN = "middleNameEn";
+    final static String FULL_SPECIALITY_NAME = "specialityName";
+    final static String FACULTY_NAME= "facultyName";
+    final static String DOCUMENT_SERIES = "documentSeries";
+    final static String DOCUMENT_NUMBER = "documentNumber";
+    final static String AWARD_TYPE_ID = "awardTypeId";
+    final static String EDUCATION_ID = "educationId";
 }
