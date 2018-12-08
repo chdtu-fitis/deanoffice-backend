266913332163447b90e0c295086db4a752abd849
Vlad Muzychenko <pexterexpert@gmail.com>
2018-12-08 17:36:51 +0200

    add: diploma numb synch: formed methods for add to green(synchronized data) and red(missing data) lists in EdeboDiplomaSynchronizationReport

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/diploma/number/EdeboDiplomaNumberSynchronizationReport.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/diploma/number/EdeboDiplomaNumberSynchronizationReport.java
@@ -1,4 +1,28 @@
 package ua.edu.chdtu.deanoffice.service.datasync.edebo.diploma.number;
 
+import lombok.Getter;
+import lombok.Setter;
+import ua.edu.chdtu.deanoffice.service.datasync.edebo.diploma.number.beans.DiplomaAndStudentSynchronizedDataBean;
+import ua.edu.chdtu.deanoffice.service.datasync.edebo.diploma.number.beans.MissingDataBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 public class EdeboDiplomaNumberSynchronizationReport {
+    private List<DiplomaAndStudentSynchronizedDataBean> diplomaAndStudentSynchronizedDataBeans;
+    private List<MissingDataBean> missingDataBeans;
+
+    public EdeboDiplomaNumberSynchronizationReport(){
+        diplomaAndStudentSynchronizedDataBeans = new ArrayList();
+        missingDataBeans = new ArrayList();
+    }
+
+    public void addBeanToSynchronizedList(DiplomaAndStudentSynchronizedDataBean bean){
+        diplomaAndStudentSynchronizedDataBeans.add(bean);
+    }
+    public void addBeanToMissingDataList(MissingDataBean bean){
+        missingDataBeans.add(bean);
+    }
 }
