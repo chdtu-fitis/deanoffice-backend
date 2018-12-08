824b499e2b8f928166daeef63272f33d9f218eae
Vlad Muzychenko <pexterexpert@gmail.com>
2018-12-08 11:56:43 +0200

    add: diploma numb synch: formed DiplomaImportData

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/diploma/number/DiplomaImportData.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/diploma/number/DiplomaImportData.java
@@ -1,4 +1,36 @@
 package ua.edu.chdtu.deanoffice.service.datasync.edebo.diploma.number;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DiplomaImportData {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String firstNameEn;
+    private String lastNameEn;
+    private String middleNameEn;
+    private String specialityName;
+    private String facultyName;
+    private String documentSeries;
+    private String documentNumber;
+    private String awardTypeId;
+    private String educationId;
+
+    DiplomaImportData(){
+        firstName = "";
+        lastName = "";
+        middleName = "";
+        firstNameEn = "";
+        lastNameEn = "";
+        middleNameEn = "";
+        specialityName = "";
+        facultyName = "";
+        documentSeries = "";
+        documentNumber = "";
+        awardTypeId = "";
+        educationId = "";
+    }
 }
