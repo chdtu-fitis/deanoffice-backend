5e557829a991e2833104ee1c3ee6e30f48299845
gzaspa <gzaspa@ukr.net>
2018-12-26 11:33:26 +0200

    develop: DebtorStudentDegreesBean according to existing user table

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/stipend/DebtorStudentDegreesBean.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/stipend/DebtorStudentDegreesBean.java
@@ -2,6 +2,9 @@ package ua.edu.chdtu.deanoffice.service.stipend;
 
 import lombok.Getter;
 import lombok.Setter;
+import ua.edu.chdtu.deanoffice.entity.Course;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,14 +13,14 @@ public class DebtorStudentDegreesBean {
     private String surname;
     private String name;
     private String patronimic;
+    private String degreeName;
     private String groupName;
+    private int year;
+    private String tuitionTerm;
     private String code;
     private String specialityName;
     private String specializationName;
-    private String courseName;
-    private String knowledgeControlName;
-    private String semester;
-    private String points;
-    private String grade;
-    private String ects;
+    private String departmentAbbreviation;
+    private double averageGrade;
+    private List<Course> debtCourses;
 }
