8202bf3288580ae02a4e54982d5e2707020e19cb
Alex22438 <Alex222438@gmail.com>
2019-02-05 15:35:26 +0200

    add: debtors statistics: controller returns mock result

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/report/debtor/DebtorReportController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/report/debtor/DebtorReportController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.edu.chdtu.deanoffice.entity.ApplicationUser;
 import ua.edu.chdtu.deanoffice.service.report.debtor.DebtorReportService;
+import ua.edu.chdtu.deanoffice.service.report.debtor.SpecializationDebtorsBean;
+import ua.edu.chdtu.deanoffice.service.report.debtor.SpecializationDebtorsYearBean;
 import ua.edu.chdtu.deanoffice.webstarter.security.CurrentUser;
+import ua.edu.chdtu.deanoffice.api.general.mapper.Mapper;
 
 import java.util.Map;
 import java.util.TreeMap;
@@ -27,6 +30,34 @@ public class DebtorReportController {
     public ResponseEntity<Map<String, SpecializationDebtorStatisticsDto>> getReportOfDebtors(@CurrentUser ApplicationUser user) {
         try {
             Map<String, SpecializationDebtorStatisticsDto> debtorsReport = new TreeMap<>(); //debtorReportService.calculateDebtorsReportData(user.getFaculty().getId());
+//             = debtorReportService.calculateDebtorsReportData(user.getFaculty());
+//            Mapper.map(, debtorsReport);
+            Map<Integer, SpecializationDebtorsYearBean> testMap = new TreeMap<>();
+            SpecializationDebtorsYearBean specializationDebtorsYearBeanTest = new SpecializationDebtorsYearBean(20, 30, 4, 6, 20, 3, 4,1,2);
+            testMap.put(1, specializationDebtorsYearBeanTest);
+            SpecializationDebtorsYearBean specializationDebtorsYearBeanTest2 = new SpecializationDebtorsYearBean(40,60,8,12,20,6,8,2,4);
+            testMap.put(2, specializationDebtorsYearBeanTest2);
+            SpecializationDebtorsBean specializationDebtorsBean = new SpecializationDebtorsBean();
+            specializationDebtorsBean.setSpecializationDebtorsYearBeanMap(testMap);
+
+            testMap.clear();
+
+            SpecializationDebtorsYearBean specializationDebtorsYearBeanTest3 = new SpecializationDebtorsYearBean(30,30,20,25,75,16, 13,4,12);
+            testMap.put(1, specializationDebtorsYearBeanTest3);
+            SpecializationDebtorsYearBean specializationDebtorsYearBeanTest4 = new SpecializationDebtorsYearBean(60,60,40,50,75,32, 26,8,24);
+            testMap.put(2, specializationDebtorsYearBeanTest4);
+            SpecializationDebtorsBean specializationDebtorsBean2 = new SpecializationDebtorsBean();
+            specializationDebtorsBean2.setSpecializationDebtorsYearBeanMap(testMap);
+
+            Map<String, SpecializationDebtorsBean> sourceDebtorsReport = new TreeMap<>();
+            sourceDebtorsReport.put("Інженерія програмного забезпечення", specializationDebtorsBean);
+            SpecializationDebtorStatisticsDto sds1 = new SpecializationDebtorStatisticsDto();
+            Mapper.map(specializationDebtorsBean, sds1);
+            debtorsReport.put("test specialization1", sds1);
+
+            SpecializationDebtorStatisticsDto sds2 = new SpecializationDebtorStatisticsDto();
+            Mapper.map(specializationDebtorsBean2, sds2);
+            debtorsReport.put("test specialization2", sds2);
 
             return ResponseEntity.ok().body(debtorsReport);
         } catch (Exception e) {
