a2e90863c995cf34c77fe85e0af06e05095b9f4c
NicolasFaydik <787898faydikripper>
2019-02-11 11:39:54 +0200

    create: Renewed Expelled Student Service

---
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/RenewedExpelledStudentService.java
@@ -0,0 +1,23 @@
+package ua.edu.chdtu.deanoffice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.edu.chdtu.deanoffice.entity.RenewedExpelledStudent;
+import ua.edu.chdtu.deanoffice.repository.RenewedExpelledStudentRepository;
+
+@Service
+public class RenewedExpelledStudentService {
+    private final RenewedExpelledStudentRepository renewedExpelledStudentRepository;
+
+    @Autowired
+    public RenewedExpelledStudentService(
+            RenewedExpelledStudentRepository renewedExpelledStudentRepository
+    ) {
+        this.renewedExpelledStudentRepository = renewedExpelledStudentRepository;
+    }
+
+    public RenewedExpelledStudent getRenewedStudentByExpelledId(Integer expelId){
+        RenewedExpelledStudent renewedExpelledStudentInformation = renewedExpelledStudentRepository.findRenewedStudentByExpelId(expelId);
+        return renewedExpelledStudentInformation;
+    }
+}
