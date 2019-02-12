420c0a8406c7cb5b399c7135395f22b07a99ef9f
SashaKononenko <alex327021@gmail.com>
2019-02-12 23:23:09 +0200

    first working version

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/document/report/academicdifference/AcademicDifferenceService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/document/report/academicdifference/AcademicDifferenceService.java
@@ -10,16 +10,14 @@ import org.springframework.stereotype.Service;
 import ua.edu.chdtu.deanoffice.entity.Grade;
 import ua.edu.chdtu.deanoffice.entity.Student;
 import ua.edu.chdtu.deanoffice.entity.StudentDegree;
+import ua.edu.chdtu.deanoffice.service.GradeService;
 import ua.edu.chdtu.deanoffice.service.StudentDegreeService;
 import ua.edu.chdtu.deanoffice.service.document.DocumentIOService;
 import ua.edu.chdtu.deanoffice.service.document.FileFormatEnum;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static ua.edu.chdtu.deanoffice.service.document.DocumentIOService.TEMPLATES_PATH;
 import static ua.edu.chdtu.deanoffice.service.document.TemplateUtil.*;
@@ -31,6 +29,9 @@ public class AcademicDifferenceService {
     private DocumentIOService documentIOService;
     @Autowired
     private StudentDegreeService studentDegreeService;
+    @Autowired
+    private GradeService gradeService;
+
     public File formDocument(int studentDegreeId) throws Docx4JException, IOException {
         StudentDegree studentDegree = studentDegreeService.getById(studentDegreeId);
         WordprocessingMLPackage resultTemplate = formDocument(TEMPLATE,studentDegree);
@@ -40,7 +41,7 @@ public class AcademicDifferenceService {
     private WordprocessingMLPackage formDocument(String templateFilepath, StudentDegree studentDegree)
             throws Docx4JException {
         WordprocessingMLPackage template = documentIOService.loadTemplate(templateFilepath);
-        prepareTable(template);
+        prepareTable(template,studentDegree);
         HashMap<String, String> result = new HashMap();
         result.put("name", studentDegree.getStudent().getInitialsUkr());
         result.put("group", studentDegree.getStudentGroup().getName());
@@ -48,32 +49,47 @@ public class AcademicDifferenceService {
         return template;
     }
 
-    private void prepareTable(WordprocessingMLPackage template) {
+    private void prepareTable(WordprocessingMLPackage template,StudentDegree studentDegree) {
         Tbl table = findTable(template, "#num");
-        prepareRows(table);
+        Map<Integer,List<UnpassedSubject>> semesters = new HashMap();;
+        List<List<Grade>> grades = gradeService.getGradesByStudentDegreeId(studentDegree.getId());
+        for(List<Grade> GradesWithСertainKC:grades){
+            for(Grade grade: GradesWithСertainKC){
+                if (grade.getGrade() == null || grade.getGrade() == 0){
+                    int numberSemester = grade.getCourse().getSemester();
+                    if (semesters.get(numberSemester) == null){
+                        semesters.put(numberSemester,new ArrayList<UnpassedSubject>());
+                    }
+                    UnpassedSubject unpassedSubject = new UnpassedSubject(
+                        grade.getCourse().getCourseName().getName(),
+                        grade.getCourse().getHours(),
+                        grade.getCourse().getKnowledgeControl().getName());
+                    semesters.get(numberSemester).add(unpassedSubject);
+                }
+
+            }
+        }
+        prepareRows(table,semesters);
     }
 
-    private void prepareRows(Tbl table) {
+    private void prepareRows(Tbl table, Map<Integer,List<UnpassedSubject>> semesters) {
         List<Tr> tableRows = (List<Tr>) (Object) getAllElementsFromObject(table, Tr.class);
         Tr rowWithWordSemestr = tableRows.get(1);
         Tr rowWithCourse =  tableRows.get(2);
         table.getContent().remove(2);
         table.getContent().remove(1);
         int row = 1;
-        for (int i=1;i<3;i++){
-            HashMap<String, String> semestr = new HashMap();
-            semestr.put("num",String.valueOf(i));
+        for ( int key:semesters.keySet()){
+
+            HashMap<String, String> title = new HashMap();
+            title.put("num",String.valueOf(key));
             Tr newRowWithSignature = XmlUtils.deepCopy(rowWithWordSemestr);
-            replaceInRow(newRowWithSignature, semestr);
+            replaceInRow(newRowWithSignature, title);
             table.getContent().add(row,newRowWithSignature);
             row++;
-            for (int j=1;j<3;j++){
-                HashMap<String, String> courses = new HashMap();
-                courses.put("n","Subject");
-                courses.put("h","120");
-                courses.put("v","Залік");
+            for (UnpassedSubject unpassedSubject:semesters.get(key)){
                 Tr newRow = XmlUtils.deepCopy(rowWithCourse);
-                replaceInRow(newRow, courses);
+                replaceInRow(newRow, unpassedSubject.getDictionary());
                 table.getContent().add(row,newRow);
                 row++;
             }
