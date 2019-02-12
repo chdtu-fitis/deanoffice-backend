28b5ba10a4cbfcbd51dcdb1af2846c9e9c52443a
Vlad Muzychenko <pexterexpert@gmail.com>
2019-02-12 08:42:04 +0200

    refactor: pre university synch: rename DEDUCTION_DATE_REGEXP on EXPEL_DATE_REGEXP in EdeboStudentDataSynchronizationServiceImpl

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/student/EdeboStudentDataSynchronizationServiceImpl.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/student/EdeboStudentDataSynchronizationServiceImpl.java
@@ -53,7 +53,7 @@ public class EdeboStudentDataSynchronizationServiceImpl implements EdeboStudentD
     private static final String SPECIALITY_REGEXP_OLD = "([\\d]\\.[\\d]+)\\s([\\w\\W]+)";
     private static final String SPECIALITY_REGEXP_NEW = "([\\d]{3})\\s([\\w\\W]+)";
     private static final String ADMISSION_REGEXP = "Номер[\\s]+наказу[\\s:]+([\\w\\W]+);[\\W\\w]+Дата[\\s]+наказу[\\s:]*([0-9]{2}.[0-9]{2}.[0-9]{4})";
-    private static final String DEDUCTION_DATE_REGEXP = "[\\W\\w]+Дата[\\s]+відрахування[\\s:]*([0-9]{2}.[0-9]{2}.[0-9]{4})";
+    private static final String EXPEL_DATE_REGEXP = "[\\W\\w]+Дата[\\s]+відрахування[\\s:]*([0-9]{2}.[0-9]{2}.[0-9]{4})";
     private static final String STUDENT_PREVIOUS_UNIVERSITY_FIELDS_TO_COMPARE[] = {"universityName", "studyStartDate", "studyEndDate"};
     private static final String SECONDARY_STUDENT_DEGREE_FIELDS_TO_COMPARE[] = {"payment", "previousDiplomaNumber", "previousDiplomaDate",
             "previousDiplomaType", "previousDiplomaIssuedBy", "supplementNumber", "admissionDate", "admissionOrderNumber", "admissionOrderDate"};
@@ -291,7 +291,7 @@ public class EdeboStudentDataSynchronizationServiceImpl implements EdeboStudentD
     private Date getDeductionDateFromPreviousUniversity(String eduFromInfo) {
         Date deductionDateFromPreviousUniversity = null;
         DateFormat deductionDateFormatter = new SimpleDateFormat("dd.MM.yyyy");
-        Pattern pattern = Pattern.compile(DEDUCTION_DATE_REGEXP);
+        Pattern pattern = Pattern.compile(EXPEL_DATE_REGEXP);
         Matcher matcher = pattern.matcher(eduFromInfo);
         try {
             if (matcher.find()) {
