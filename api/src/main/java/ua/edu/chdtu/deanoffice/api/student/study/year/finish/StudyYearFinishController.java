4648c8580abb77fa8ca882616ace2a5875d5cb40
YulyKo <juliakotenko.forstudy@gmail.com>
2019-03-04 16:39:22 +0200

    issue - 207 : change and fix StudyYearFinishController and add one new method handleException; add DTO class StudyYearFinishDTO

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/student/study/year/finish/StudyYearFinishController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/student/study/year/finish/StudyYearFinishController.java
@@ -3,9 +3,14 @@ package ua.edu.chdtu.deanoffice.api.student.study.year.finish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.edu.chdtu.deanoffice.api.general.ExceptionHandlerAdvice;
+import ua.edu.chdtu.deanoffice.api.general.ExceptionToHttpCodeMapUtil;
+import ua.edu.chdtu.deanoffice.api.student.synchronization.diploma.number.DiplomaNumberController;
 import ua.edu.chdtu.deanoffice.entity.ApplicationUser;
+import ua.edu.chdtu.deanoffice.exception.OperationCannotBePerformedException;
 import ua.edu.chdtu.deanoffice.service.DataVerificationService;
 import ua.edu.chdtu.deanoffice.service.StudentDegreeService;
 import ua.edu.chdtu.deanoffice.service.StudentExpelService;
@@ -32,17 +37,24 @@ public class StudyYearFinishController {
         this.dataVerificationService = dataVerificationService;
     }
 
-    @PostMapping
-    public ResponseEntity expelStudents(List<Integer> ids, Date expelDate, Date orderDate, int orderNumber, @CurrentUser ApplicationUser user){
+    @PostMapping("/expel-students")
+    public ResponseEntity expelStudents(@RequestBody StudyYearFinishDTO studyYearFinishDTO, @CurrentUser ApplicationUser user){
         try {
-            facultyAuthorizationService.verifyAccessibilityOfStudentDegrees(ids, user);
-            if (dataVerificationService.isStudentDegreesActiveByIds(ids)) {
-                throw new Exception();
+            facultyAuthorizationService.verifyAccessibilityOfStudentDegrees(studyYearFinishDTO.getIds(), user);
+            if (!dataVerificationService.isStudentDegreesActiveByIds(studyYearFinishDTO.getIds())) {
+                throw new OperationCannotBePerformedException("Серед даних студентів є неактивні");
             }
 
+            studyYearFinishService.expelStudents(studyYearFinishDTO.getIds(), studyYearFinishDTO.getExpelDate(), studyYearFinishDTO.getOrderDate(), studyYearFinishDTO.getOrderNumber());
+
         } catch (Exception e) {
-            e.printStackTrace();
+            handleException(e);
         }
 
+        return ResponseEntity.ok().build();
+    }
+
+    private ResponseEntity handleException(Exception exception) {
+        return ExceptionHandlerAdvice.handleException(exception, StudyYearFinishController.class, ExceptionToHttpCodeMapUtil.map(exception));
     }
 }
