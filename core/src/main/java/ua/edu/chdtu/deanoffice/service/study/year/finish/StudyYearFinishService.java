6bf6caa10fda9f6bd597702c4631e483d594675e
YulyKo <juliakotenko.forstudy@gmail.com>
2019-03-05 11:16:35 +0200

    develop

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/study/year/finish/StudyYearFinishService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/study/year/finish/StudyYearFinishService.java
@@ -3,9 +3,11 @@ package ua.edu.chdtu.deanoffice.service.study.year.finish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.edu.chdtu.deanoffice.entity.StudentDegree;
+import ua.edu.chdtu.deanoffice.exception.OperationCannotBePerformedException;
 import ua.edu.chdtu.deanoffice.service.StudentDegreeService;
 import ua.edu.chdtu.deanoffice.service.StudentExpelService;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -21,8 +23,21 @@ public class StudyYearFinishService {
         this.studentExpelService = studentExpelService;
     }
 
-    public void expelStudents(List<Integer> ids, Date expelDate, Date orderDate, String orderNumber) throws Exception {
+    public void expelStudents(List<StudentDegree> studentDegrees, Date expelDate, Date orderDate, String orderNumber) throws Exception {
             //studentDegreeService.setStudentDegreesInactive(ids);
-            studentExpelService.expelStudents(ids, expelDate, orderDate, orderNumber);
+            //studentDegreeRepository.getAllByIds(ids);
+
+
+            studentExpelService.expelStudents(studentDegrees, expelDate, orderDate, orderNumber);
+
+            List<Integer> groups = new ArrayList<>();
+
+            for (StudentDegree studentDegree : studentDegrees) {
+                if (studentDegree.getStudentGroup().isActive() == true) {
+
+                } else {
+                    throw new OperationCannotBePerformedException("Студенти");
+                }
+            }
     }
 }
