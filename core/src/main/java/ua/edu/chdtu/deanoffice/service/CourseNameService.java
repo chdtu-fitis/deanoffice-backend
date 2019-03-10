173f9a847be8ac85825f7058bb51c2b0c6c2fce2
Andrew Kon <andriykonogray@gmail.com>
2019-03-10 05:01:24 +0200

    delete excessive class

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/CourseNameService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/CourseNameService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.edu.chdtu.deanoffice.entity.CourseName;
 import ua.edu.chdtu.deanoffice.repository.CourseNameRepository;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,21 +33,16 @@ public class CourseNameService {
     public Map<String, String> getAllCoursesWhereEngNameIsNullOrEmpty(int facultyId, int degreeId) {
         int year = currentYearService.getYear();
         List<Object[]> coursesName = courseNameRepository.findAllForGraduates(year, facultyId, degreeId);
-        List<CourseNameWithGroupNameAndSemester> courseNameWithGroupNameAndSemesters = new ArrayList<>(coursesName.size());
-        coursesName.forEach(item ->
-            courseNameWithGroupNameAndSemesters.add(
-                    new CourseNameWithGroupNameAndSemester((String)item[0], (String)item[1],
-                                                            (String)item[2], (Integer) item[3])));
-        return courseNameWithGroupNameAndSemesters
+        return coursesName
                 .stream()
                 .filter(cn -> !checkNameEng(cn).equals(""))
-                .collect(Collectors.toMap(cn -> cn.getGroupName(), this::checkNameEng, (e1, e2) -> e1, LinkedHashMap::new));
+                .collect(Collectors.toMap(cn -> (String)cn[0], this::checkNameEng, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
-    private String checkNameEng(CourseNameWithGroupNameAndSemester cn){
+    private String checkNameEng(Object item[]){
         String message = "";
-        message += Strings.isNullOrEmpty(cn.getNameEng()) ? String.format("Група: %s Семестр: %d",
-                cn.getGroupName(), cn.getSemester()) : "";
+        message += Strings.isNullOrEmpty((String)item[1]) ? String.format("Група: %s Семестр: %d",
+                (String) item[2], (Integer) item[3]) : "";
         return message;
     }
 }
