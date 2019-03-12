6248de113890b1e10b9b72080dee90aef87c4678
Rostik Kravchenko <mr.markinger@gmail.com>
2019-03-12 10:00:17 +0200

    add: CourseRepository : findStudentGroupsMatchingForeignGroups

--- core/src/main/java/ua/edu/chdtu/deanoffice/repository/StudentGroupRepository.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/repository/StudentGroupRepository.java
@@ -63,7 +63,7 @@ public interface StudentGroupRepository extends JpaRepository<StudentGroup, Inte
             "and sg.specialization.degree.id = :degree_id " +
             "and :curr_year - sg.creationYear + sg.beginYears = :study_year " +
             "and sg.specialization.faculty.id = :faculty_id " +
-            "and sg.tuitionForm = :tuitionForm "+
+            "and sg.tuitionForm = :tuitionForm " +
             "order by sg.name")
     List<StudentGroup> findGroupsByDegreeAndYearAndTuitionForm(
             @Param("degree_id") Integer degreeId,
@@ -91,7 +91,7 @@ public interface StudentGroupRepository extends JpaRepository<StudentGroup, Inte
             "where sg.specialization.id = :specialization_id " +
             "and sg.id = :student_group_id " +
             "order by sg.name")
-    List <StudentGroup> findAllBySpecializationIdAndGroupId(
+    List<StudentGroup> findAllBySpecializationIdAndGroupId(
             @Param("specialization_id") Integer specializationId,
             @Param("student_group_id") Integer studentGroupId
     );
@@ -112,4 +112,11 @@ public interface StudentGroupRepository extends JpaRepository<StudentGroup, Inte
             "WHERE sg.specialization.id = :specializationId " +
             "and sg.active = true")
     List<StudentGroup> findBySpecializationId(@Param("specializationId") int specializationId);
+
+    @Query("select sg from StudentGroup sg " +
+            "where active = :active and sg.name in " +
+            "(select substring(fg.name,1,length(fg.name) - 2) from StudentGroup fg " +
+            "where fg.specialization.faculty.id = 8 and fg.active = :active)"
+    )
+    List<StudentGroup> findStudentGroupsMatchingForeignGroups(@Param("active") Boolean active);
 }
