81e80177c7126b1f2c38fe82510a35956495a111
Rostik Kravchenko <mr.markinger@gmail.com>
2019-03-19 13:32:06 +0200

    add: courseName: repository operations

--- core/src/main/java/ua/edu/chdtu/deanoffice/repository/CourseNameRepository.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/repository/CourseNameRepository.java
@@ -1,9 +1,20 @@
 package ua.edu.chdtu.deanoffice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import ua.edu.chdtu.deanoffice.entity.CourseName;
+
+import java.util.List;
 
 public interface CourseNameRepository extends JpaRepository<CourseName, Integer> {
     CourseName findByName(String name);
+
+    @Query("select cn from CourseName as cn " +
+            "where cn.id not in(select distinct c.courseName.id from Course as c)")
+    List<CourseName> findUnusedCoursesNames();
+
+    @Transactional
+    void deleteCourseNameById(List<Integer> ids);
 
 }
