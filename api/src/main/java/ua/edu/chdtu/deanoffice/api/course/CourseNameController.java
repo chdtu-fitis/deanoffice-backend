58c0e8e505d26dc8552f5a4987f91874ac819a91
Rostik Kravchenko <mr.markinger@gmail.com>
2019-03-22 15:27:40 +0200

    enhance: courseName: methods mapping

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/course/CourseNameController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/course/CourseNameController.java
@@ -3,6 +3,8 @@ package ua.edu.chdtu.deanoffice.api.course;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ua.edu.chdtu.deanoffice.api.course.dto.CourseNameDTO;
@@ -19,6 +21,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/courses-names")
 public class CourseNameController {
     private CourseNameService courseNameService;
 
@@ -26,7 +29,7 @@ public class CourseNameController {
         this.courseNameService = courseNameService;
     }
 
-    @GetMapping("/courses-names/unused")
+    @GetMapping("/unused")
     public ResponseEntity getUnusedCoursesNames() {
         try {
             List<CourseName> unusedCoursesNames = courseNameService.getUnusedCoursesNames();
@@ -36,7 +39,7 @@ public class CourseNameController {
         }
     }
 
-    @DeleteMapping("/courses-names")
+    @DeleteMapping
     public ResponseEntity deleteCoursesNamesByIds(@RequestParam("ids") List<Integer> ids) {
         try {
             courseNameService.deleteCoursesNamesByIds(ids);
@@ -46,7 +49,7 @@ public class CourseNameController {
         }
     }
 
-    @GetMapping("/similar-courses-names")
+    @GetMapping("/similar")
     public ResponseEntity getSimilarCoursesNames() {
         Map<CourseName, List<CourseName>> similarCoursesNames = courseNameService.getSimilarCoursesNames();
         List<List<CourseName>> mappedList = new ArrayList<>();
