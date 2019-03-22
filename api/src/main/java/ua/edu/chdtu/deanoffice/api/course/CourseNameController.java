6f6d6528a0ad83562c1d8f28970ad5e227b074fd
Rostik Kravchenko <mr.markinger@gmail.com>
2019-03-22 15:20:47 +0200

    add: courseName: method for getting similar courses names

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/course/CourseNameController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/course/CourseNameController.java
@@ -12,7 +12,11 @@ import ua.edu.chdtu.deanoffice.api.general.mapper.Mapper;
 import ua.edu.chdtu.deanoffice.entity.CourseName;
 import ua.edu.chdtu.deanoffice.service.course.CourseNameService;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class CourseNameController {
@@ -40,6 +44,24 @@ public class CourseNameController {
         } catch (Exception exception) {
             return handleException(exception);
         }
+    }
+
+    @GetMapping("/similar-courses-names")
+    public ResponseEntity getSimilarCoursesNames() {
+        Map<CourseName, List<CourseName>> similarCoursesNames = courseNameService.getSimilarCoursesNames();
+        List<List<CourseName>> mappedList = new ArrayList<>();
+        similarCoursesNames.keySet()
+                .forEach(key -> {
+                    ArrayList<CourseName> currentList = new ArrayList<>();
+                    currentList.add(key);
+                    currentList.addAll(similarCoursesNames.get(key));
+                    mappedList.add(currentList);
+                });
+        List<List<CourseNameDTO>> resultDTOs = new ArrayList<>();
+        for (List<CourseName> list : mappedList) {
+            resultDTOs.add(Mapper.map(list, CourseNameDTO.class));
+        }
+        return ResponseEntity.ok(resultDTOs);
     }
 
     private ResponseEntity handleException(Exception exception) {
