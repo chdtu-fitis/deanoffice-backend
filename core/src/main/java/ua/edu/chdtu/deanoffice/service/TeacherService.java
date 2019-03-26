699ad00c37861598efbb54504927ae27288ad76b
YulyKo <juliakotenko.forstudy@gmail.com>
2019-03-26 16:26:51 +0200

    change method "getTeachersByIds" to "getTeachersByActive"; add one method "save";

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/TeacherService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/TeacherService.java
@@ -2,6 +2,7 @@ package ua.edu.chdtu.deanoffice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ua.edu.chdtu.deanoffice.entity.Teacher;
 import ua.edu.chdtu.deanoffice.repository.TeacherRepository;
 
@@ -19,8 +20,16 @@ public class TeacherService {
         return teacherRepository.findAllByOrderBySurname();
     }
 
-    public List<Teacher> getTeachersByIds(List<Integer> ids) {
-        return teacherRepository.findAllByIds(ids);
+//    public List<Teacher> getTeachersByIds(List<Integer> ids) {
+//        return teacherRepository.findAllByIds(ids);
+//    }
+
+    public List<Teacher> getTeachersByActive(boolean active) {
+        return teacherRepository.findAllByActive(active);
+    }
+
+    public void save(Teacher teacher) {
+        teacherRepository.save(teacher);
     }
 
     public void deleteTeachers(List<Integer> ids) {
