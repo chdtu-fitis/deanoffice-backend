4dd5cf08a8678531e339188b43915e87d51a9b16
YulyKo <juliakotenko.forstudy@gmail.com>
2019-03-27 16:38:22 +0200

    fix: teachers: correct object fields names

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/teacher/TeacherDTO.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/teacher/TeacherDTO.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import ua.edu.chdtu.deanoffice.api.general.dto.DepartmentDTO;
 import ua.edu.chdtu.deanoffice.api.general.dto.NamedDTO;
+import ua.edu.chdtu.deanoffice.entity.Position;
 import ua.edu.chdtu.deanoffice.entity.superclasses.Sex;
 
 @Getter
@@ -16,6 +17,6 @@ public class TeacherDTO {
     private String surname;
     private boolean active;
     private String scientificDegree;
-    private DepartmentDTO departmentDto;
-    private NamedDTO positionDto;
+    private DepartmentDTO department;
+    private NamedDTO position;
 }
