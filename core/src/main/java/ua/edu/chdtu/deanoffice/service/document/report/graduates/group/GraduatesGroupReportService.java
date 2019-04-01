e31abd5ce24f684be9c7d7a7cd05d63a38896b65
Rostik Kravchenko <mr.markinger@gmail.com>
2019-04-01 22:55:52 +0300

    244-add: GraduatesGroupReport: service

---
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/document/report/graduates/group/GraduatesGroupReportService.java
@@ -0,0 +1,8 @@
+package ua.edu.chdtu.deanoffice.service.document.report.graduates.group;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GraduatesGroupReportService {
+
+}
