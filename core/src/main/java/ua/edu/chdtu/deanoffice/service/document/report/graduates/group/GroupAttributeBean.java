f08c738bea5d6e4ce51091e8dc838be980a0910d
Rostik Kravchenko <mr.markinger@gmail.com>
2019-04-02 14:43:12 +0300

    244-add:GroupAttributeBean.class

---
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/document/report/graduates/group/GroupAttributeBean.java
@@ -0,0 +1,14 @@
+package ua.edu.chdtu.deanoffice.service.document.report.graduates.group;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class GroupAttributeBean {
+    private String degree;
+    private String studyTerm;
+    private String speciality;
+    private String specialization;
+    private String academicGroup;
+}
