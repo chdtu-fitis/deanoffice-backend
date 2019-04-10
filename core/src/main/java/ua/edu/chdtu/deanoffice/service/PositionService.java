6080afeeca3bce17cbc2c20a930b4da0c943094e
BlackMaizeGod <artuom.logv@gmail.com>
2019-04-10 15:58:18 +0300

    #248 teachers parameters check: enhance: add method getById

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/PositionService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/PositionService.java
@@ -2,6 +2,7 @@ package ua.edu.chdtu.deanoffice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ua.edu.chdtu.deanoffice.entity.Position;
 import ua.edu.chdtu.deanoffice.repository.PositionRepository;
 
 @Service
@@ -11,5 +12,9 @@ public class PositionService {
     @Autowired
     public PositionService(PositionRepository positionRepository) {
         this.positionRepository = positionRepository;
+    }
+
+    public Position getById(Integer positionId) {
+        return positionRepository.findOne(positionId);
     }
 }
