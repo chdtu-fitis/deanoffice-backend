324c0e9ad9ed399b99884d0475fd23c52826c56d
Artem Sinkovskyi <artem.sinkovskiy@gmail.com>
2019-04-17 18:29:26 +0300

    #260_issue: add StudentSurnameChange (Entity)

---
+++ core/src/main/java/ua/edu/chdtu/deanoffice/entity/StudentSurnameChange.java
@@ -0,0 +1,59 @@
+package ua.edu.chdtu.deanoffice.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import ua.edu.chdtu.deanoffice.entity.superclasses.BaseEntity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Getter
+@Setter
+@Entity
+public class StudentSurnameChange extends BaseEntity {
+    @Column(nullable = false)
+    Date orderDate;
+
+    @Column(nullable = false)
+    String orderNumber;
+
+    @ManyToOne
+    Faculty facultyId;
+
+    @ManyToOne
+    StudentDegree studentDegreeId;
+
+    @Column(nullable = false)
+    Date surnameChangeDate;
+
+    @Column(nullable = false)
+    String specialityName;
+
+    @Column(nullable = false)
+    String specializationName;
+
+    @Column(nullable = false)
+    Integer studentYear;
+
+    @Column(nullable = false)
+    String studentGroupName;
+
+    @Column(nullable = false)
+    String tuitionForm;
+
+    @Column(nullable = false)
+    String payment;
+
+    @Column(nullable = false)
+    Date applicationDate;
+
+    @Column(nullable = false)
+    String applicationBasedOn;
+
+    @Column(nullable = false)
+    String oldSurname;
+
+    @Column(nullable = false)
+    String newSurname;
+}
