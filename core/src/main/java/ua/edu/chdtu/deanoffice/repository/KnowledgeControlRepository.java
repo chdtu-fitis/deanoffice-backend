9cef16ce99d79a28db734a60f37c45bfe62aa35e
NicolasFaydik <787898faydikripper>
2019-04-19 10:16:05 +0300

    create: method findGradedByKnowledgeControlId

--- core/src/main/java/ua/edu/chdtu/deanoffice/repository/KnowledgeControlRepository.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/repository/KnowledgeControlRepository.java
@@ -1,7 +1,12 @@
 package ua.edu.chdtu.deanoffice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.edu.chdtu.deanoffice.entity.KnowledgeControl;
 
 public interface KnowledgeControlRepository extends JpaRepository<KnowledgeControl, Integer> {
+    @Query("SELECT graded from KnowledgeControl kc " +
+            "where kc.id = :kc_id ")
+    boolean findGradedByKnowledgeControlId(@Param("kc_id") int kc_id);
 }
