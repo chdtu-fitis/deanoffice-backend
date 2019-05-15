7b94a94bb888f20ab73f0ce5273b0d8e0988826f
Ростислав <mr.markinger@gmail.com>
2019-05-15 17:42:51 +0300

    #266 UserRoles: add: Role class

---
+++ core/src/main/java/ua/edu/chdtu/deanoffice/entity/Role.java
@@ -0,0 +1,24 @@
+package ua.edu.chdtu.deanoffice.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+}
