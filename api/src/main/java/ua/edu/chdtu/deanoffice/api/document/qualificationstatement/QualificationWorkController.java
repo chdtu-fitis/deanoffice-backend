170b5ad1b583558ee89b414e5b0bb523a1c91483
Vlad Muzychenko <pexterexpert@gmail.com>
2019-09-25 18:56:56 +0300

    #318 statement of qualification work: add: add QualificationWorkController

---
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/document/qualificationstatement/QualificationWorkController.java
@@ -0,0 +1,18 @@
+package ua.edu.chdtu.deanoffice.api.document.qualificationstatement;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.edu.chdtu.deanoffice.api.general.ExceptionHandlerAdvice;
+import ua.edu.chdtu.deanoffice.api.general.ExceptionToHttpCodeMapUtil;
+
+@RestController
+@RequestMapping("documents/statement")
+public class QualificationWorkController {
+
+    public QualificationWorkController() {
+
+    }
+
+}
+
