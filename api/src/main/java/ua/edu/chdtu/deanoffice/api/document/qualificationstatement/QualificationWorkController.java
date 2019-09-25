076446b55543d113a9925e42a0fdefd9ec90eca9
Vlad Muzychenko <pexterexpert@gmail.com>
2019-09-25 19:03:52 +0300

    #318 statement of qualification work: refactor: changed request mapping for QualificationWorkController

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/document/qualificationstatement/QualificationWorkController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/document/qualificationstatement/QualificationWorkController.java
@@ -1,13 +1,15 @@
 package ua.edu.chdtu.deanoffice.api.document.qualificationstatement;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionHandlerAdvice;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionToHttpCodeMapUtil;
 
 @RestController
-@RequestMapping("documents/statement")
+@RequestMapping("/documents/statement")
 public class QualificationWorkController {
 
     public QualificationWorkController() {
