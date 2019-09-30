283e970cbce1417a40d10cdaca9462dd1723706b
Vlad Muzychenko <pexterexpert@gmail.com>
2019-09-30 14:10:54 +0300

    #318 statement of qualification work: add: getStatementOfQualificationWorkPdf method

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/document/qualificationstatement/QualificationWorkController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/document/qualificationstatement/QualificationWorkController.java
@@ -1,19 +1,36 @@
 package ua.edu.chdtu.deanoffice.api.document.qualificationstatement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.edu.chdtu.deanoffice.api.document.DocumentResponseController;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionHandlerAdvice;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionToHttpCodeMapUtil;
+import ua.edu.chdtu.deanoffice.service.document.report.qualificationstatement.QualificationWorkService;
+
+import java.io.File;
 
 @RestController
 @RequestMapping("/documents/statement")
-public class QualificationWorkController {
+public class QualificationWorkController  extends DocumentResponseController {
+    private QualificationWorkService qualificationWorkService;
 
-    public QualificationWorkController() {
+    @Autowired
+    public QualificationWorkController(QualificationWorkService qualificationWorkService) {
+        this.qualificationWorkService = qualificationWorkService;
+    }
 
+    @GetMapping("/{groupId}/qualification/work")
+    public ResponseEntity getStatementOfQualificationWorkPdf(@PathVariable int groupId) {
+        try {
+            File file = qualificationWorkService.createQualificationWorkStatementForGroup(groupId);
+            return buildDocumentResponseEntity(file, file.getName(), MEDIA_TYPE_PDF);
+        } catch (Exception exception) {
+            return handleException(exception);
+        }
     }
 
     private ResponseEntity handleException(Exception exception) {
