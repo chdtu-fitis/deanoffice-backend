b5aaecd9d19362218b210633344f63b26b23702b
Vlad Muzychenko <pexterexpert@gmail.com>
2019-10-29 19:52:15 +0200

    #318 statement of qualification work: enhancement: changed path variable for group id to request param

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/document/qualificationstatement/QualificationWorkReportController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/document/qualificationstatement/QualificationWorkReportController.java
@@ -2,10 +2,7 @@ package ua.edu.chdtu.deanoffice.api.document.qualificationstatement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ua.edu.chdtu.deanoffice.api.document.DocumentResponseController;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionHandlerAdvice;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionToHttpCodeMapUtil;
@@ -23,8 +20,8 @@ public class QualificationWorkReportController extends DocumentResponseControlle
         this.qualificationWorkReportService = qualificationWorkReportService;
     }
 
-    @GetMapping("/{groupId}/qualification/work")
-    public ResponseEntity getStatementOfQualificationWorkPdf(@PathVariable int groupId) {
+    @GetMapping("/qualification/work")
+    public ResponseEntity getStatementOfQualificationWorkPdf(@RequestParam Integer groupId) {
         try {
             File file = qualificationWorkReportService.createQualificationWorkStatementForGroup(groupId);
             return buildDocumentResponseEntity(file, file.getName(), MEDIA_TYPE_PDF);
