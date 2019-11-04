39414a027db824282d26b02737d213b33f892331
WiperGrap <bilonigviktor@gmail.com>
2019-11-04 17:36:25 +0200

    #367 not enough space for faculty and group name:bug fix.

--- core/src/main/java/ua/edu/chdtu/deanoffice/util/FacultyUtil.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/util/FacultyUtil.java
@@ -1,6 +1,7 @@
 package ua.edu.chdtu.deanoffice.util;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import ua.edu.chdtu.deanoffice.entity.Faculty;
 
 public class FacultyUtil {
     public static String getUserFacultyId() {
@@ -9,5 +10,12 @@ public class FacultyUtil {
 
     public static int getUserFacultyIdInt() {
         return Integer.parseInt((String) SecurityContextHolder.getContext().getAuthentication().getDetails());
+    }
+
+    public static String getRefinedFacultyName(Faculty faculty) {
+        String defaultFacultyName = faculty.getName();
+        String fixedFacultyName = defaultFacultyName.replace("факультет","").trim();
+
+        return fixedFacultyName;
     }
 }
