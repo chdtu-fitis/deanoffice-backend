44ed4e0838917ba83963d30ebe8afdb8100e21e6
gzaspa <gzaspa@ukr.net>
2019-11-06 11:26:56 +0200

    #266 add user roles: fix: add all user roles to roles list

--- api/src/main/java/ua/edu/chdtu/deanoffice/webstarter/security/CurrentUserDetails.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/webstarter/security/CurrentUserDetails.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import ua.edu.chdtu.deanoffice.entity.ApplicationUser;
+import ua.edu.chdtu.deanoffice.entity.Role;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +22,10 @@ public class CurrentUserDetails extends org.springframework.security.core.userde
     }
 
     static List<GrantedAuthority> get(ApplicationUser user) {
-        List<GrantedAuthority> a = new ArrayList<>();
-        a.add(new SimpleGrantedAuthority(user.getRoles().get(0).getName()));
-        return a;
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Role role: user.getRoles())
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        return grantedAuthorities;
     }
 }
 
