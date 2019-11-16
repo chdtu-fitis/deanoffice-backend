8fdf22b2df8be0669e504b95805706a40056c8e1
Михайло Мироненко <4mainbrain@gmail.com>
2019-11-16 16:30:54 +0200

    #334-feat/add_tuition_form_term_to_student_degree

--- core/src/main/resources/db/migration/V31__add-tuition_form_and_term_to_student_degree.sql
+++ core/src/main/resources/db/migration/V31__add-tuition_form_and_term_to_student_degree.sql
@@ -9,7 +9,12 @@ SET tuition_form = student_group.tuition_form,
 FROM student_group
 WHERE student_group.id = student_degree.student_group_id;
 
-UPDATE student_degree set tuition_form ='FULL_TIME'
+UPDATE student_degree SET tuition_form ='FULL_TIME'
 WHERE tuition_form IS NULL;
-UPDATE student_degree set tuition_term = 'REGULAR'
+UPDATE student_degree SET tuition_term = 'REGULAR'
 WHERE tuition_term IS NULL;
+
+ALTER TABLE student_degree
+ALTER COLUMN tuition_form SET NOT NULL ;
+ALTER TABLE student_degree
+ALTER COLUMN tuition_term SET NOT NULL;
