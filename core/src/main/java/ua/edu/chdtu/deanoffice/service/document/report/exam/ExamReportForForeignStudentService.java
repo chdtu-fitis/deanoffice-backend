ed30ae5a70b34dfc1a911692784bfc7270da97fb
DianaBabenko <baben.k.o.diana2017@gmail.com>
2019-12-20 05:08:34 +0200

    #336 exam-report-for-foreign-student : change methods

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/document/report/exam/ExamReportForForeignStudentService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/document/report/exam/ExamReportForForeignStudentService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.edu.chdtu.deanoffice.Constants;
 import ua.edu.chdtu.deanoffice.entity.*;
-//import ua.edu.chdtu.deanoffice.entity.*;
 import ua.edu.chdtu.deanoffice.service.CourseForGroupService;
 import ua.edu.chdtu.deanoffice.service.CurrentYearService;
 import ua.edu.chdtu.deanoffice.service.StudentDegreeService;
@@ -17,8 +16,6 @@ import ua.edu.chdtu.deanoffice.service.document.DocumentIOService;
 import ua.edu.chdtu.deanoffice.service.document.FileFormatEnum;
 import ua.edu.chdtu.deanoffice.service.document.TemplateUtil;
 import ua.edu.chdtu.deanoffice.util.LanguageUtil;
-
-
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
@@ -50,10 +47,12 @@ public class ExamReportForForeignStudentService extends ExamReportBaseService {
         super(currentYearService);
         this.documentIOService = documentIOService;
     }
-//+- іменування файлу
+
     public File createGroupStatementForeign(List<Integer> studentIds, int semester, FileFormatEnum format)
             throws Exception {
         if (studentIds.size() > 0) {
+            String fileName = "";
+            int i = 0;
             List<StudentDegree> studentDegrees = studentDegreeService.getByIds(studentIds);
             Map<StudentDegree, List<CourseForGroup>> studentsAndCourses = new HashMap<>();
             for(StudentDegree studentDegree : studentDegrees) {
@@ -63,16 +62,19 @@ public class ExamReportForForeignStudentService extends ExamReportBaseService {
                     List<CourseForGroup> coursesForGroups = courseForGroupService.getCoursesForGroupBySemester(groupId,semester);
                     studentsAndCourses.put(studentDegree, coursesForGroups);
                 }
+                if (i < 2) {
+                    fileName += LanguageUtil.transliterate(studentDegree.getStudent().getSurname() + studentDegree.getStudentGroup());
+                    i++;
+                }
             }
-            String fileName = LanguageUtil.transliterate("student");
             WordprocessingMLPackage filledTemplate = fillTemplate(TEMPLATE, studentsAndCourses);
             return documentIOService.saveDocumentToTemp(filledTemplate, fileName, format);
 
         } else throw new Exception();
     }
-//+-
+
     WordprocessingMLPackage fillTemplate(String templateName, Map<StudentDegree, List<CourseForGroup>> coursesToStudents)
-            throws IOException, Docx4JException { //головний метод
+            throws IOException, Docx4JException {
         WordprocessingMLPackage reportsDocument = documentIOService.loadTemplate(templateName);
         int i = 0;
             for (Map.Entry<StudentDegree, List<CourseForGroup>> coursesToStudent : coursesToStudents.entrySet()) {
@@ -88,29 +90,23 @@ public class ExamReportForForeignStudentService extends ExamReportBaseService {
                     e.printStackTrace();
                 }
             }
-        //}
         return reportsDocument;
     }
-//+
+
     WordprocessingMLPackage fillTemplate(String templateName, StudentDegree studentDegree, List<CourseForGroup> coursesToStudent) //генерує для одного студента з багатьма предметами
             throws IOException, Docx4JException {
         WordprocessingMLPackage template = documentIOService.loadTemplate(templateName);
         Map<String, String> commonDict = new HashMap<>();
-        commonDict.putAll(getStudentInfoReplacements(studentDegree)); //////////
+        commonDict.putAll(getStudentInfoReplacements(studentDegree));
 
         fillTableWithCoursesInfo(template, coursesToStudent);
         TemplateUtil.replaceTextPlaceholdersInTemplate(template, commonDict);
-        //Tbl tempTable = TemplateUtil.getAllTablesFromDocument(template).get(0);
-        //template.getMainDocumentPart().getContent().remove(1);
         return template;
     }
 
     public void fillTemplate(WordprocessingMLPackage template, StudentDegree studentDegree, List<CourseForGroup> coursesToStudent) // формування інф за 1 студ із предметами
             throws IOException, Docx4JException {
-        //int currentRowIndex = STARTING_ROW_INDEX;
-        //for (CourseForGroup courseForGroup : coursesToStudent) {
         fillTableWithCoursesInfo(template, coursesToStudent);// заповнить інформацію за предмети
-        //currentRowIndex += coursesToStudent.size() + 1;
         removeUnfilledPlaceholders(template);
         Map<String, String> commonDict = new HashMap<>();
                 commonDict.putAll(getSemester(coursesToStudent.get(0).getCourse().getSemester()));
@@ -128,7 +124,6 @@ public class ExamReportForForeignStudentService extends ExamReportBaseService {
         int numberOfRow = 1;
         Tr blanckRow = (Tr) gradeTableRows.get(currentRowIndex);
 
-        //Tr newRowWithSignature = XmlUtils.deepCopy(blanckRow);
         for (CourseForGroup courseForGroup : courses) {
             Tr currentRow = XmlUtils.deepCopy(blanckRow);
 
@@ -136,7 +131,6 @@ public class ExamReportForForeignStudentService extends ExamReportBaseService {
             Map<String, String> replacements = new HashMap<>();
             replacements.put("n", String.format("" + numberOfRow));
             replacements.put("CourseName", course.getCourseName().getName());
-           // replacements.put("Semester", String.format("%d-й",courseForGroup.getCourse().getSemester()));/////
             replacements.put("KCType", course.getKnowledgeControl().getName());
             replacements.put("Hours", String.format("%d", course.getHours()));
             replacements.put("TeacherInitials", courseForGroup.getTeacher().getInitialsUkr());
@@ -144,7 +138,6 @@ public class ExamReportForForeignStudentService extends ExamReportBaseService {
             tempTable.getContent().add(currentRowIndex, currentRow);
             currentRowIndex++;
             numberOfRow++;
-            //replaceInRow(currentRow,replacements);
         }
         removeUnfilledPlaceholders(template);
         tempTable.getContent().remove(currentRowIndex);
@@ -159,7 +152,6 @@ public class ExamReportForForeignStudentService extends ExamReportBaseService {
     private void removeUnfilledPlaceholders(WordprocessingMLPackage template) {
         Set<String> placeholdersToRemove = new HashSet<>();
         placeholdersToRemove.add("CourseName");
-       // placeholdersToRemove.add("Semester");////
         placeholdersToRemove.add("KCType");
         placeholdersToRemove.add("Hours");
         placeholdersToRemove.add("TeacherInitials");
@@ -172,17 +164,13 @@ public class ExamReportForForeignStudentService extends ExamReportBaseService {
         result.put("GroupName", studentDegree.getStudentGroup().getName());
         Speciality speciality = studentDegree.getSpecialization().getSpeciality();
         result.put("Speciality", speciality.getCode() + " " + speciality.getName());
-        result.put("FacultyAbbr", studentDegree.getSpecialization().getFaculty().getAbbr());
-        result.put("DeanInitials", makeInitialsSurnameLast(studentDegree.getSpecialization().getFaculty().getDean().toString()));/////
+        result.put("FAbbr", studentDegree.getSpecialization().getFaculty().getAbbr());
+        result.put("DeanInitials", makeInitialsSurnameLast(studentDegree.getSpecialization().getFaculty().getDean()));/////
         result.put("StudyYear", getStudyYear());
         result.put("StudentInitials", studentDegree.getStudent().getFullNameUkr());
         result.put("RecBook", studentDegree.getRecordBookNumber());
         result.put("Course",  String.format("%d", currentYear - studentDegree.getStudentGroup().getCreationYear() + studentDegree.getStudentGroup().getBeginYears()));
-
         return result;
     }
-
-
-
 }
 
