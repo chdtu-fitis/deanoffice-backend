541d31d5c09a2ea1147cf2ef257dee06102a49ef
gzaspa <mshvenra5>
2020-05-07 21:29:36 +0300

    orders: develop: creation flyway script (general order tables)

--- core/src/main/resources/db/migration/V34__add_orders.sql
+++ core/src/main/resources/db/migration/V34__add_orders.sql
@@ -1,18 +1,24 @@
 -- Used for cases when an order template changes (and we need to give user an previously generated version)
-create table order_version
+-- document_type - тип документа (ордер про відрахування, про переведення  тощо)
+-- template_name -  версія темплейта ордера - форма документа може змінюваатись з часом
+create table order_template_version
 (
     id            SERIAL primary key,
-    template_name varchar not null,
-    document_type varchar not null,
+    template_name varchar(20) not null,
+    document_type varchar(25) not null,
+    introduced_on date not null,
     active        boolean
 );
+
+ALTER TABLE ONLY order_template_version
+    ADD CONSTRAINT uk_document_type_and_introduced_on UNIQUE (document_type, introduced_on);
 -- People who are listed in the bottom of each order (approvers). Will be used to create a set of approvers
 -- (order_approve_template). May be improved with faculty id inclusion (to restrict user from seeing templates of other faculties)
 create table order_single_approver
 (
     id       SERIAL primary key,
-    position varchar,
-    initials varchar
+    position varchar(150) not null,
+    full_name varchar(40) not null
 );
 
 -- User will choose among templates to paste suitable one. Needs discussion on structure and further sequencing in order.
@@ -24,13 +30,13 @@ create table order_approve_template
     introduced_by_id     int,
     approver_id          int,
     faculty_id           int,
-    active               bit
+    active               boolean
 );
 
 ALTER TABLE order_approve_template
     ADD CONSTRAINT fk_execution_control_id FOREIGN KEY (execution_control_id) REFERENCES order_single_approver (id);
 ALTER TABLE order_approve_template
-    ADD CONSTRAINT head_approver_id FOREIGN KEY (head_approver_id) REFERENCES order_single_approver (id);
+    ADD CONSTRAINT fk_head_approver_id FOREIGN KEY (head_approver_id) REFERENCES order_single_approver (id);
 ALTER TABLE order_approve_template
     ADD CONSTRAINT fk_introduced_by_id FOREIGN KEY (introduced_by_id) REFERENCES order_single_approver (id);
 ALTER TABLE order_approve_template
@@ -46,11 +52,11 @@ ALTER TABLE order_approve_template
 create table orders
 (
     id                        SERIAL primary key,
-    order_version_id          int     not null,
+    order_template_version_id int     not null,
     faculty_id                int     not null,
     order_date                date    not null,
-    order_name                varchar not null,
-    order_reason              int     not null,
+    order_number              varchar(15) not null,
+    order_type_id             int     not null,
     order_approve_template_id int     not null,
     comment                   varchar,
     active                    boolean,
@@ -58,13 +64,13 @@ create table orders
 );
 
 ALTER TABLE orders
-    ADD CONSTRAINT fk_orders_order_version_id FOREIGN KEY (order_version_id) REFERENCES order_version (id);
+    ADD CONSTRAINT fk_orders_order_template_version_id FOREIGN KEY (order_template_version_id) REFERENCES order_templateq_version (id);
 ALTER TABLE orders
     ADD CONSTRAINT fk_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
 ALTER TABLE orders
     ADD CONSTRAINT fk_approver_template_id FOREIGN KEY (order_approve_template_id) REFERENCES order_approve_template (id);
 ALTER TABLE orders
-    ADD CONSTRAINT fk_order_reason_id FOREIGN KEY (order_reason) REFERENCES order_reason (id);
+    ADD CONSTRAINT fk_order_type_id FOREIGN KEY (order_type_id) REFERENCES order_type (id);
 
 
 -- Information inputted by user while order generation. Used both for order all order generations (read&write)
@@ -74,17 +80,6 @@ create table order_user_input_set
     order_id   int not null,
     user_input json
 );
-
--- mapping that matches template and approver set
-create table order_approve_template_mapping
-(
-    order_id                  int,
-    order_approve_template_id int
-);
-ALTER TABLE order_approve_template_mapping
-    ADD CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders (id);
-ALTER TABLE order_approve_template_mapping
-    ADD CONSTRAINT fk_order_approve_template_id FOREIGN KEY (order_approve_template_id) REFERENCES order_approve_template (id);
 
 ALTER TABLE order_user_input_set
     ADD CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders (id);
