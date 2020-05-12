c70f62269bfd1c94ce56068991501e9e0998eabf
gzaspa <mshvenra5>
2020-05-12 17:21:39 +0300

    orders: add: tables to flyway script; first draft version

--- core/src/main/resources/db/migration/V36__add_orders.sql
+++ core/src/main/resources/db/migration/V36__add_orders.sql
@@ -37,43 +37,50 @@ ALTER TABLE legal_basis
 -- (order_approve_template). May be improved with faculty id inclusion (to restrict user from seeing templates of other faculties)
 create table order_single_approver
 (
-    id       SERIAL primary key,
+    id SERIAL primary key,
     position varchar(150) not null,
-    full_name varchar(40) not null
+    full_name varchar(40) not null,
+    faculty_id integer,
+    active boolean
 );
 ALTER TABLE order_single_approver
     ADD CONSTRAINT uk_order_single_approver_position_and_full_name UNIQUE (position, full_name);
+ALTER TABLE order_single_approver
+    ADD CONSTRAINT fk_order_single_approver_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
 
 -- User will choose among templates to paste suitable one. Needs discussion on structure and further sequencing in order.
 create table order_approve_template
 (
-    id                   SERIAL primary key,
-    execution_control_id int,
-    head_approver_id     int,
-    introduced_by_id     int,
-    approver_id          int,
-    faculty_id           int,
-    active               boolean
+    id SERIAL primary key,
+    main_approver_id integer not null,
+    university_approver_ids integer[] not null,
+    faculty_approver_ids integer[] not null,
+    initiator_approver integer not null,
+    faculty_id integer not null,
+    active     boolea nnot null
 );
-
 ALTER TABLE order_approve_template
-    ADD CONSTRAINT fk_execution_control_id FOREIGN KEY (execution_control_id) REFERENCES order_single_approver (id);
-ALTER TABLE order_approve_template
-    ADD CONSTRAINT fk_head_approver_id FOREIGN KEY (head_approver_id) REFERENCES order_single_approver (id);
-ALTER TABLE order_approve_template
-    ADD CONSTRAINT fk_introduced_by_id FOREIGN KEY (introduced_by_id) REFERENCES order_single_approver (id);
-ALTER TABLE order_approve_template
-    ADD CONSTRAINT fk_approver_id FOREIGN KEY (approver_id) REFERENCES order_single_approver (id);
-ALTER TABLE order_approve_template
-    ADD CONSTRAINT fk_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
+    ADD CONSTRAINT fk_order_approve_template_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
 -- ALTER TABLE order_approve_template
---     ADD CONSTRAINT uk_order_approve_template_ UNIQUE ();
+--     ADD CONSTRAINT uk_order_approve_template_approvers UNIQUE (approvers);
+CREATE UNIQUE INDEX uk_order_approve_template_approvers ON order_approve_template( (approvers->>'main_approver_id, university_approvers') ) ;
 
+create table order_control_template
+(
+    id SERIAL primary key,
+    control_text varchar(300),
+    faculty_id   integer,
+    active       boolean
+);
+ALTER TABLE order_control_template
+    ADD CONSTRAINT fk_order_control_template_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
+ALTER TABLE order_control_template
+    ADD CONSTRAINT uk_order_control_template_control_text UNIQUE (control_text);
 -- Main business object,=.
 -- active == true -> if order has not been deleted (cannot be in signed status).
 -- active == true, signed == false -> order is in draft status.
 -- active == true, signed == true -> order's business logic has been applied
-
+-- order_control - зазвичай останній абзац у наказі - контроль за виконанням наказу покласти на...
 create table orders
 (
     id                        SERIAL primary key,
@@ -83,7 +90,8 @@ create table orders
     order_number              varchar(15) not null,
     order_type_id             int     not null,
     order_approve_template_id int     not null,
-    comment                   varchar,
+    order_control_id          integer,
+    comment                   varchar(200),
     active                    boolean,
     signed                    boolean
 );
@@ -99,13 +107,45 @@ ALTER TABLE orders
 ALTER TABLE orders
     ADD CONSTRAINT uk_order_faculty_id_and_order_number_and_order_date UNIQUE (faculty_id, order_number, order_date);
 
--- Information inputted by user while order generation. Used both for order all order generations (read&write)
-create table order_item
+-- Paragraphs in order; can be in sections
+ALTER TABLE student_expel ADD COLUMN order_id integer not null;
+ALTER TABLE student_expel ADD COLUMN tuition_form varchar(10) not null;
+ALTER TABLE student_expel ADD COLUMN tuition_term varchar(10) not null;
+ALTER TABLE student_expel ADD COLUMN specialization_id integer not null;
+ALTER TABLE student_expel ADD COLUMN speciality_id integer not null;
+ALTER TABLE student_expel ADD COLUMN reason_document varchar(150) not null;
+ALTER TABLE student_expel ADD COLUMN section varchar(50) not null default '';
+ALTER TABLE student_expel ADD COLUMN item_text varchar(1000) not null;
+
+ALTER TABLE student_expel ADD CONSTRAINT fk_student_expel_order_id FOREIGN KEY (order_id) REFERENCES orders (id);
+ALTER TABLE student_expel ADD CONSTRAINT fk_student_expel_specialization_id FOREIGN KEY (specialization_id) REFERENCES specialization (id);
+ALTER TABLE student_expel ADD CONSTRAINT fk_student_expel_speciality_id FOREIGN KEY (speciality_id) REFERENCES speciality (id);
+ALTER TABLE orders ADD CONSTRAINT uk_student_expel_order_id_student_degree_id UNIQUE (order_id, student_degree_id);
+
+create table name_cases
 (
-    id         SERIAL primary key,
-    order_id   int not null,
-    user_input json
+    id SERIAL primary key,
+    nominative    varchar(25),
+    genitive      varchar(25),
+    dative        varchar(25),
+    accusative    varchar(25),
+    instrumental  varchar(25),
+    prepositional varchar(25),
 );
 
-ALTER TABLE order_user_input_set
-    ADD CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders (id);
+create table male_surname_cases
+(
+    id SERIAL primary key,
+    male_nominative   varchar(25),
+    female_nominative varchar(25),
+    male_genitive     varchar(25),
+    female_genitive   varchar(25),
+    male_dative       varchar(25),
+    female_dative     varchar(25),
+    male_accusative    varchar(25),
+    female_accusative    varchar(25),
+    male_instrumental  varchar(25),
+    female_instrumental  varchar(25),
+    male_prepositional varchar(25),
+    female_prepositional varchar(25),
+);
