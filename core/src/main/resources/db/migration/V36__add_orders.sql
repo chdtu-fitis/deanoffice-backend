f24bd887fcf7c9f4e9530cf2709fb7bed0834275
gzaspa <mshvenra5>
2020-05-12 22:10:04 +0300

    orders: develop: tables to flyway script; finished first draft version

--- core/src/main/resources/db/migration/V36__add_orders.sql
+++ core/src/main/resources/db/migration/V36__add_orders.sql
@@ -1,14 +1,15 @@
--- Види наказів: ордер про відрахування, про переведення  тощо
+-- Види наказів: наказ про відрахування, про переведення тощо
 create table order_type
 (
     id            SERIAL primary key,
     db_table_name varchar(50) not null,
     text_in_order varchar(100) not null,
     introduced_on date not null,
-    active        boolean
+    active        boolean not null default true
 );
 ALTER TABLE order_type
     ADD CONSTRAINT uk_order_type_db_table_name_and_introduced_on UNIQUE (db_table_name, introduced_on);
+
 -- Used for cases when an order template changes (and we need to give user an previously generated version)
 -- template_name -  версія темплейта ордера(docx) - форма документа може змінюваатись з часом
 create table order_template_version
@@ -17,8 +18,10 @@ create table order_template_version
     template_name varchar(40) not null,
     order_type_id int not null,
     introduced_on date not null,
-    active        boolean
+    active        boolean not null default true
 );
+ALTER TABLE order_template_version
+    ADD CONSTRAINT fk_order_template_version_order_type_id FOREIGN KEY (order_type_id) REFERENCES order_type (id);
 ALTER TABLE order_template_version
     ADD CONSTRAINT uk_order_template_version_order_type_and_introduced_on UNIQUE (order_type_id, introduced_on);
 
@@ -27,26 +30,26 @@ create table legal_basis
 (
     id SERIAL primary key,
     legal_basis_text varchar(600) not null,
-    introduced_on date not null,
-    active        boolean
+    introduced_on    date not null,
+    active           boolean not null default true
 );
 ALTER TABLE legal_basis
     ADD CONSTRAINT uk_legal_basis_text_and_introduced_on UNIQUE (legal_basis_text, introduced_on);
 
 -- People who are listed in the bottom of each order (approvers). Will be used to create a set of approvers
 -- (order_approve_template). May be improved with faculty id inclusion (to restrict user from seeing templates of other faculties)
-create table order_single_approver
+create table order_approver
 (
     id SERIAL primary key,
     position varchar(150) not null,
-    full_name varchar(40) not null,
+    full_name varchar(50) not null,
     faculty_id integer,
-    active boolean
+    active boolean not null default true
 );
-ALTER TABLE order_single_approver
-    ADD CONSTRAINT uk_order_single_approver_position_and_full_name UNIQUE (position, full_name);
-ALTER TABLE order_single_approver
-    ADD CONSTRAINT fk_order_single_approver_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
+ALTER TABLE order_approver
+    ADD CONSTRAINT fk_order_approver_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
+ALTER TABLE order_approver
+    ADD CONSTRAINT uk_order_approver_position_and_full_name UNIQUE (position, full_name);
 
 -- User will choose among templates to paste suitable one. Needs discussion on structure and further sequencing in order.
 create table order_approve_template
@@ -55,27 +58,32 @@ create table order_approve_template
     main_approver_id integer not null,
     university_approver_ids integer[] not null,
     faculty_approver_ids integer[] not null,
-    initiator_approver integer not null,
+    initiator_approver_id integer not null,
     faculty_id integer not null,
-    active     boolea nnot null
+    active boolean not null default true
 );
+ALTER TABLE order_approve_template
+    ADD CONSTRAINT fk_order_approve_template_main_approver_id FOREIGN KEY (main_approver_id) REFERENCES order_approver (id);
+ALTER TABLE order_approve_template
+    ADD CONSTRAINT fk_order_approve_template_initiator_approver_id FOREIGN KEY (initiator_approver_id) REFERENCES order_approver (id);
 ALTER TABLE order_approve_template
     ADD CONSTRAINT fk_order_approve_template_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
 -- ALTER TABLE order_approve_template
 --     ADD CONSTRAINT uk_order_approve_template_approvers UNIQUE (approvers);
-CREATE UNIQUE INDEX uk_order_approve_template_approvers ON order_approve_template( (approvers->>'main_approver_id, university_approvers') ) ;
 
+-- Абзац про те, хто контролюватиме виконання наказу
 create table order_control_template
 (
     id SERIAL primary key,
     control_text varchar(300),
-    faculty_id   integer,
-    active       boolean
+    faculty_id   integer not null,
+    active       boolean not null default true
 );
 ALTER TABLE order_control_template
     ADD CONSTRAINT fk_order_control_template_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
 ALTER TABLE order_control_template
     ADD CONSTRAINT uk_order_control_template_control_text UNIQUE (control_text);
+
 -- Main business object,=.
 -- active == true -> if order has not been deleted (cannot be in signed status).
 -- active == true, signed == false -> order is in draft status.
@@ -90,22 +98,24 @@ create table orders
     order_number              varchar(15) not null,
     order_type_id             int     not null,
     order_approve_template_id int     not null,
-    order_control_id          integer,
+    order_control_template_id integer,
     comment                   varchar(200),
-    active                    boolean,
-    signed                    boolean
+    active                    boolean not null default true,
+    signed                    boolean not null default false
 );
 
 ALTER TABLE orders
     ADD CONSTRAINT fk_orders_order_template_version_id FOREIGN KEY (order_template_version_id) REFERENCES order_templateq_version (id);
 ALTER TABLE orders
-    ADD CONSTRAINT fk_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
+    ADD CONSTRAINT fk_orders_faculty_id FOREIGN KEY (faculty_id) REFERENCES faculty (id);
 ALTER TABLE orders
-    ADD CONSTRAINT fk_approver_template_id FOREIGN KEY (order_approve_template_id) REFERENCES order_approve_template (id);
+    ADD CONSTRAINT fk_orders_order_type_id FOREIGN KEY (order_type_id) REFERENCES order_type (id);
 ALTER TABLE orders
-    ADD CONSTRAINT fk_order_type_id FOREIGN KEY (order_type_id) REFERENCES order_type (id);
+    ADD CONSTRAINT fk_orders_approve_template_id FOREIGN KEY (order_approve_template_id) REFERENCES order_approve_template (id);
 ALTER TABLE orders
-    ADD CONSTRAINT uk_order_faculty_id_and_order_number_and_order_date UNIQUE (faculty_id, order_number, order_date);
+    ADD CONSTRAINT fk_orders_order_control_template_id FOREIGN KEY (order_control_template_id) REFERENCES order_control_template (id);
+ALTER TABLE orders
+    ADD CONSTRAINT uk_orders_faculty_id_and_order_number_and_order_date UNIQUE (faculty_id, order_number, order_date);
 
 -- Paragraphs in order; can be in sections
 ALTER TABLE student_expel ADD COLUMN order_id integer not null;
@@ -120,7 +130,7 @@ ALTER TABLE student_expel ADD COLUMN item_text varchar(1000) not null;
 ALTER TABLE student_expel ADD CONSTRAINT fk_student_expel_order_id FOREIGN KEY (order_id) REFERENCES orders (id);
 ALTER TABLE student_expel ADD CONSTRAINT fk_student_expel_specialization_id FOREIGN KEY (specialization_id) REFERENCES specialization (id);
 ALTER TABLE student_expel ADD CONSTRAINT fk_student_expel_speciality_id FOREIGN KEY (speciality_id) REFERENCES speciality (id);
-ALTER TABLE orders ADD CONSTRAINT uk_student_expel_order_id_student_degree_id UNIQUE (order_id, student_degree_id);
+ALTER TABLE student_expel ADD CONSTRAINT uk_student_expel_order_id_student_degree_id UNIQUE (order_id, student_degree_id);
 
 create table name_cases
 (
@@ -133,7 +143,7 @@ create table name_cases
     prepositional varchar(25),
 );
 
-create table male_surname_cases
+create table surname_cases
 (
     id SERIAL primary key,
     male_nominative   varchar(25),
