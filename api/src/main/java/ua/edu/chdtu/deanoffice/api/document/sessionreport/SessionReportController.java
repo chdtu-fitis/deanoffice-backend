bf4a225d9f0726c58da4183d7ac2f4e0e8bd5135
Alex222438 <alex222438@gmail.com>
2020-06-01 00:12:25 +0300

    #324 session report in excel: add: added new package and class - controller

---
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/document/sessionreport/SessionReportController.java
@@ -0,0 +1,12 @@
+package ua.edu.chdtu.deanoffice.api.document.sessionreport;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/documents/session-report")
+public class SessionReportController {
+
+    
+
+}
