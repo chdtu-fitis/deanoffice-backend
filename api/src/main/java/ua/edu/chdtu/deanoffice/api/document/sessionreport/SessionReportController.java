d2ffb2bba3456e78271837c920ad1ebd7985f047
Alex222438 <alex222438@gmail.com>
2020-06-01 00:16:53 +0300

    #324 session report in excel: add: add field and constructor to controller

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/document/sessionreport/SessionReportController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/document/sessionreport/SessionReportController.java
@@ -1,12 +1,19 @@
 package ua.edu.chdtu.deanoffice.api.document.sessionreport;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.edu.chdtu.deanoffice.service.document.sessionreport.SessionReportService;
 
 @RestController
 @RequestMapping("/documents/session-report")
 public class SessionReportController {
 
-
+    private final SessionReportService sessionReportService;
+    
+    @Autowired
+    public SessionReportController(SessionReportService sessionReportService) {
+        this.sessionReportService = sessionReportService;
+    }
 
 }
