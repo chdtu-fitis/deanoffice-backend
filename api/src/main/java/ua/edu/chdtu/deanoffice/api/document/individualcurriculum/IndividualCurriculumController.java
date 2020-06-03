94226ae8842835b3336b5997212daa62fd168bb7
gzaspa <mshvenra5>
2020-06-03 10:18:50 +0300

    #404 individual curriculum: develop: remove groupId and put studentDegreeIds instead of studentIds in controller parameters

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/document/individualcurriculum/IndividualCurriculumController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/document/individualcurriculum/IndividualCurriculumController.java
@@ -4,6 +4,7 @@ import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,15 +24,14 @@ public class IndividualCurriculumController extends DocumentResponseController {
         this.individualCurriculumService = individualCurriculumService;
     }
 
-    @GetMapping
+    @GetMapping("/{studyYear}/docx")
     public ResponseEntity generateIndividualCurriculum(
-            @RequestParam(required = false) int groupId,
-            @RequestParam(required = false) List<Integer> studentsIds,
-            @RequestParam String studyYear
+            @RequestParam List<Integer> studentDegreeIds,
+            @PathVariable Integer studyYear
     ) {
         File file = null;
         try {
-            file = individualCurriculumService.createIndividualCurriculumDocx(groupId, studentsIds, studyYear);
+            file = individualCurriculumService.createIndividualCurriculumDocx(studentDegreeIds, ""+studyYear);
         } catch (FileNotFoundException | Docx4JException e) {
             e.printStackTrace();
         }
--- core/src/main/java/ua/edu/chdtu/deanoffice/service/document/individualcurriculum/IndividualCurriculumService.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/document/individualcurriculum/IndividualCurriculumService.java
@@ -4,77 +4,70 @@ import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.springframework.stereotype.Service;
 import ua.edu.chdtu.deanoffice.entity.StudentDegree;
-import ua.edu.chdtu.deanoffice.service.StudentDegreeService;
+import ua.edu.chdtu.deanoffice.repository.StudentDegreeRepository;
 import ua.edu.chdtu.deanoffice.service.document.DocumentIOService;
 import ua.edu.chdtu.deanoffice.service.document.FileFormatEnum;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 
 @Service
 public class IndividualCurriculumService {
-    private static final String FILE_NAME = "Individual_curriculum_of_the_higher_educators";
+    private static final String FILE_NAME = "Individual_curriculum";
 
-    private final StudentDegreeService studentDegreeService;
+    private final StudentDegreeRepository studentDegreeRepository;
     private final IndividualCurriculumFillTemplateService fillTemplateService;
     private final DocumentIOService documentIOService;
 
-    public IndividualCurriculumService(StudentDegreeService studentDegreeService,
+    public IndividualCurriculumService(StudentDegreeRepository studentDegreeRepository,
                                        IndividualCurriculumFillTemplateService fillTemplateService,
                                        DocumentIOService documentIOService) {
-        this.studentDegreeService = studentDegreeService;
+        this.studentDegreeRepository = studentDegreeRepository;
         this.fillTemplateService = fillTemplateService;
         this.documentIOService = documentIOService;
     }
 
-    public File createIndividualCurriculumDocx(int groupId,
-                                               List<Integer> studentIds,
+    public File createIndividualCurriculumDocx(List<Integer> studentDegreeIds,
                                                String studyYear) throws FileNotFoundException, Docx4JException {
-        Set<StudentDegree> studentDegrees = getStudentDegreesByGroup(groupId);
-        studentDegrees.addAll(getStudentDegreesByStudentIds(studentIds));
-
-        Set<StudentDegree> collect = getFilteredStudentDegrees(studentDegrees);
-
+        List<StudentDegree> studentDegrees = studentDegreeRepository.getAllByIds(studentDegreeIds);
+        Set<StudentDegree> collect = sortByFullName(studentDegrees);
         WordprocessingMLPackage file = fillTemplateService.fillTemplate(collect, studyYear);
-
         return documentIOService.saveDocumentToTemp(file, FILE_NAME, FileFormatEnum.DOCX);
     }
 
-    private Set<StudentDegree> getFilteredStudentDegrees(Set<StudentDegree> studentDegrees) {
+    private Set<StudentDegree> sortByFullName(List<StudentDegree> studentDegrees) {
         return studentDegrees.stream()
                 .sorted(Comparator.comparing(degree -> (
                                 degree.getStudent().getSurname() + " " +
-                                        degree.getStudent().getName() + " " +
-                                        degree.getStudent().getPatronimic()
+                                degree.getStudent().getName() + " " +
+                                degree.getStudent().getPatronimic()
                         )
                 )).collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
-    private Set<StudentDegree> getStudentDegreesByGroup(int groupId) {
-        Set<StudentDegree> degrees = new HashSet<>();
+//    private Set<StudentDegree> getStudentDegreesByGroup(int groupId) {
+//        Set<StudentDegree> degrees = new HashSet<>();
+//
+//        if (groupId > 0) {
+//            degrees.addAll(studentDegreeService.getAllByGroupId(groupId));
+//        }
+//
+//        return degrees;
+//    }
 
-        if (groupId > 0) {
-            degrees.addAll(studentDegreeService.getAllByGroupId(groupId));
-        }
-
-        return degrees;
-    }
-
-    private Set<StudentDegree> getStudentDegreesByStudentIds(List<Integer> studentIds) {
-        Set<StudentDegree> degrees = new HashSet<>();
-
-        if (Objects.nonNull(studentIds) && studentIds.size() > 0) {
-            degrees.addAll(studentDegreeService.getByStudentIds(studentIds));
-        }
-
-        return degrees;
-    }
+//    private Set<StudentDegree> getStudentDegreesByStudentIds(List<Integer> studentIds) {
+//        Set<StudentDegree> degrees = new HashSet<>();
+//
+//        if (Objects.nonNull(studentIds) && studentIds.size() > 0) {
+//            degrees.addAll(studentDegreeService.getByStudentIds(studentIds));
+//        }
+//
+//        return degrees;
+//    }
 }
