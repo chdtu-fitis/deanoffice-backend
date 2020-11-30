2096337c467c0869e8cf175b433d53d0e60b0b89
Alex222438 <alex222438@gmail.com>
2020-11-30 18:52:36 +0200

    #324 session report in excel: delete: delete a comment

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/document/sessionreport/SessionReportController.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/document/sessionreport/SessionReportController.java
@@ -2,14 +2,12 @@ package ua.edu.chdtu.deanoffice.api.document.sessionreport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ua.edu.chdtu.deanoffice.api.document.DocumentResponseController;
-import ua.edu.chdtu.deanoffice.api.document.examreport.ConsolidatedExamReportController;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionHandlerAdvice;
 import ua.edu.chdtu.deanoffice.api.general.ExceptionToHttpCodeMapUtil;
 import ua.edu.chdtu.deanoffice.entity.ApplicationUser;
@@ -18,8 +16,6 @@ import ua.edu.chdtu.deanoffice.service.document.sessionreport.SessionReportServi
 import ua.edu.chdtu.deanoffice.webstarter.security.CurrentUser;
 
 import java.io.File;
-import java.time.LocalDate;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/documents/session-report")
@@ -31,8 +27,7 @@ public class SessionReportController extends DocumentResponseController {
     public SessionReportController(SessionReportService sessionReportService) {
         this.sessionReportService = sessionReportService;
     }
-
-    //@RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate sessionStartDate
+    
     @GetMapping
     public ResponseEntity<Resource> getSessionReportInExcel(@CurrentUser ApplicationUser user,
                                                             @RequestParam int dayOfSessionStart,
