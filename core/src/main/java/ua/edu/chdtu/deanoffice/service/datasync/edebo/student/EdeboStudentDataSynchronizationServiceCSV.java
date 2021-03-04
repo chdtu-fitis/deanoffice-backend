090f3e8d78dfa8ac3d8e245a4c02442d431eedd8
gzaspa <mshvenra5>
2021-03-04 16:26:36 +0200

    student data edebo synchronization: add: trim all strings fro csv

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/student/EdeboStudentDataSynchronizationServiceCSV.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/datasync/edebo/student/EdeboStudentDataSynchronizationServiceCSV.java
@@ -12,7 +12,11 @@ import ua.edu.chdtu.deanoffice.service.StudentService;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class EdeboStudentDataSynchronizationServiceCSV extends EdeboStudentDataSynchronizationServiceImpl {
@@ -23,12 +27,40 @@ public class EdeboStudentDataSynchronizationServiceCSV extends EdeboStudentDataS
         super(null, studentService, studentDegreeService, degreeService, specialityService, specializationService, facultyService);
     }
 
-    protected List<ImportedData> getStudentDegreesFromStream(InputStream inputStream) throws IOException {
+    protected List<ImportedData> getStudentDegreesFromStream(InputStream inputStream) {
         List<ImportedData> beans = new CsvToBeanBuilder(new InputStreamReader(inputStream))
                 .withSeparator(';')
                 .withType(ImportedData.class)
                 .build()
                 .parse();
+        for (ImportedData bean : beans)
+            trimAllProperties(bean);
         return beans;
+    }
+
+    private void trimAllProperties(ImportedData importedData) {
+        try {
+            Method[] methods = importedData.getClass().getMethods();
+            Map<Method, Method> getAndSetMethods = new HashMap<>();
+            for (Method method : methods) {
+                String methodName = method.getName();
+                if (methodName.length() > 3 && methodName.startsWith("get") && !methodName.equals("getClass") && Character.isUpperCase(methodName.charAt(3))) {
+                    String propertyName = methodName.substring(3);
+                    for (Method method1 : methods) {
+                        if (method1.getName().equals("set" + propertyName)) {
+                            getAndSetMethods.put(method, method1);
+                            break;
+                        }
+                    }
+                }
+            }
+            for (Map.Entry<Method, Method> entry : getAndSetMethods.entrySet()) {
+                Method getMethod = entry.getKey();
+                Method setMethod = entry.getValue();
+                setMethod.invoke(importedData, ((String) getMethod.invoke(importedData)).trim());
+            }
+        } catch(IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
