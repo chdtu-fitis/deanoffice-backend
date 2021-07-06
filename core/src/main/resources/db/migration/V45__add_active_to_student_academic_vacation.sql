bac596e3515c5be94b8d377241c28d3a093cd77c
gzaspa <mshvenra5>
2021-07-06 12:15:23 +0300

    academic vacation: add: set false to old academic vacations in flyway script

--- core/src/main/resources/db/migration/V45__add_active_to_student_academic_vacation.sql
+++ core/src/main/resources/db/migration/V45__add_active_to_student_academic_vacation.sql
@@ -3,3 +3,5 @@ ALTER TABLE student_academic_vacation ADD COLUMN active BOOLEAN DEFAULT true NOT
 UPDATE student_academic_vacation sav SET active = false
 FROM renewed_academic_vacation_student ravs
 WHERE ravs.student_academic_vacation_id = sav.id;
+
+UPDATE student_academic_vacation SET active = false WHERE vacation_start_date < '2018-01-01'
