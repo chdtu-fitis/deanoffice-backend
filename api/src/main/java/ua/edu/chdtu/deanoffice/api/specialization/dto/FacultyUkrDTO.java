7ed60a2deb86ad37919869b6cfb4a7cc4e08d12e
gzaspa <mshvenra5>
2021-07-25 18:46:59 +0300

    student degrees: develop: get faculty data in specialization object related to student degree object

--- api/src/main/java/ua/edu/chdtu/deanoffice/api/specialization/dto/FacultyUkrDTO.java
+++ api/src/main/java/ua/edu/chdtu/deanoffice/api/specialization/dto/FacultyUkrDTO.java
@@ -1,13 +1,18 @@
 package ua.edu.chdtu.deanoffice.api.specialization.dto;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class FacultyUkrDTO {
+    @JsonView(SpecializationView.Faculty.class)
     private int id;
+    @JsonView(SpecializationView.Faculty.class)
     private String name;
+    @JsonView(SpecializationView.Faculty.class)
     private String abbr;
+    @JsonView(SpecializationView.Faculty.class)
     private String dean;
 }
