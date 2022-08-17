5015421ac66b83611f166de5a754c5f9b78ef5f0
VasyaChopovenko <vasyachopovenko@gmail.com>
2022-08-17 23:21:59 +0300

    Add selectiveCourses initialization in SelectiveCoursesStudentDegreeId model

--- core/src/main/java/ua/edu/chdtu/deanoffice/service/course/selective/SelectiveCoursesStudentDegreeId.java
+++ core/src/main/java/ua/edu/chdtu/deanoffice/service/course/selective/SelectiveCoursesStudentDegreeId.java
@@ -3,10 +3,11 @@ package ua.edu.chdtu.deanoffice.service.course.selective;
 import lombok.Data;
 import ua.edu.chdtu.deanoffice.entity.SelectiveCourse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class SelectiveCoursesStudentDegreeId {
     private ExistingId studentDegree;
-    private List<SelectiveCourse> selectiveCourses;
+    private List<SelectiveCourse> selectiveCourses = new ArrayList<>();
 }
