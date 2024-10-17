0199659f4ca996c1785007088092e9fac7f1ecc1
Valeriia Hakhova <valeriia@wsh.one>
2024-10-17 20:14:02 +0300

    #573 BaseCourse: refactor: implement base abstract class

--- src/main/java/ua/edu/chdtu/deanoffice/entity/CourseForStudent.java
+++ src/main/java/ua/edu/chdtu/deanoffice/entity/CourseForStudent.java
@@ -2,28 +2,21 @@ package ua.edu.chdtu.deanoffice.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import ua.edu.chdtu.deanoffice.entity.superclasses.BaseEntity;
+import ua.edu.chdtu.deanoffice.entity.superclasses.BaseCourse;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import java.util.Date;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "courses_for_students")
-public class CourseForStudent extends BaseEntity {
-    @ManyToOne
-    private Course course;
+public class CourseForStudent extends BaseCourse {
     @ManyToOne
     private StudentDegree studentDegree;
-    @ManyToOne
-    private Teacher teacher;
     @Enumerated(value = EnumType.STRING)
     private CourseType courseType;
     public CourseForStudent() {}
